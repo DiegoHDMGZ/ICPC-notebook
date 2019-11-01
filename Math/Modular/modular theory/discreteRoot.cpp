87fa0fd8083b0d44095dc607343ec9d69baf227c
Diego <dhurtadodemendoza@gmail.com>
2019-10-31 20:41:51 -0500

    minor change

--- Math/Modular/modular theory/discreteRoot.cpp
+++ Math/Modular/modular theory/discreteRoot.cpp
@@ -1,7 +1,7 @@
 	
 	
 	(x ^ k) % mod = r
-	Suppose we know primitve root of mod = g
+	Suppose we know primitive root of mod = g
 	( (g ^ y ) ^ k )  % mod = r
 	( (g ^ k ) ^ y ) % mod = r
 	We find y by discreteLog and then answer is g ^ y
