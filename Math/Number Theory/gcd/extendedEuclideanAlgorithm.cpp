60f972c9e97d50750ebc1a93376fa2ae3d034e9c
Diego <dhurtadodemendoza@gmail.com>
2020-02-08 23:41:38 -0500

    delete extendedEuclidean Negative

--- Math/Number Theory/gcd/extendedEuclideanAlgorithm.cpp
+++ Math/Number Theory/gcd/extendedEuclideanAlgorithm.cpp
@@ -8,16 +8,16 @@ typedef long long Long;
 
 //ax + by = gcd(a , b)
 Long gcd (Long a, Long b, Long & x, Long & y) { //O(max(loga,logb))
-    if (a == 0) {
-        x = 0;
-        y = 1;
-        return b;
-    }
-    Long x1, y1;
-    Long d = gcd (b % a, a, x1, y1);
-    x = y1 - (b / a) * x1;
-    y = x1;
-    return d;
+	if(b == 0){
+		x = 1;
+		y = 0;
+		return abs(a);
+	}
+	Long x2, y2;
+	Long g = gcd(b, a % b , x2, y2);
+	x = y2;
+	y = x2 - (a / b) * y2;
+	return g;
 }
 
 int main() {
