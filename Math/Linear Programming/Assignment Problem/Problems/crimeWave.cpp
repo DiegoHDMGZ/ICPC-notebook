5221ebe4b7a082956748e81d4155720ca2f3c335
Diego <dhurtadodemendoza@gmail.com>
2020-03-23 09:14:35 -0500

    Hungarian algorithm

---
+++ Math/Linear Programming/Assignment Problem/Problems/crimeWave.cpp
@@ -0,0 +1,132 @@
+#include <bits/stdc++.h>
+#define debug(x) cout << #x << " = " << x << endl
+#define REP(i,n) for(Long i = 0; i < (Long)n; i++)
+#define pb push_back
+using namespace std;
+
+typedef long long Long;
+typedef long double Double;
+typedef vector<vector<Double>> Matrix;
+
+const Double INF = 1e18;
+const Long MX = 1e3;
+const Long dummy = INF / 2; 
+
+/*if the objective function is max , change dummy for -INF
+and multiply the matrix by -1*/
+
+struct Hungarian{
+	Long matchRow[MX];
+	Long matchCol[MX];
+	
+	void makeSquare(Matrix &A){
+		Long n = A.size();
+		Long m = A[0].size();
+		for(Long i = 0; i < n; i++){
+			for(Long j = 0; j < n - m; j++){
+				A[i].push_back(dummy);
+			}
+		}
+	}
+	
+	Double assign(Matrix A){ //O(n^3)
+		Long n = A.size();
+		Long m = A[0].size();
+		if(n > m){
+			makeSquare(A);
+		}
+		m = n;
+
+		vector<Double> colAdd(m, 0);
+		vector<Double> rowAdd(n , 0);
+		fill(matchRow, matchRow + n, -1); //match for i-th row
+		fill(matchCol, matchCol + m , -1);//match for i-th col
+		
+		for(Long i = 0; i < n; i++){
+			vector<bool> forbidden(m, false); //forbidden column
+			vector<Long> parent(n, -1); //parent in conflict tree
+			vector<Double> minVal(m, INF); //minVal in column
+			vector<Long> minPos(m, -1); // row where the min value is achieved
+			Long cur = i;
+			while(true){
+				Long minCol = -1;
+				//update minVal and minPos and find minCol
+				for(Long j = 0; j < m; j++){
+					if(!forbidden[j]){
+						Double val = A[cur][j] + rowAdd[cur] + colAdd[j];
+						if(val < minVal[j]){
+							minVal[j] = val;
+							minPos[j] = cur;
+						}
+						if(minCol == -1 || minVal[j] < minVal[minCol]){
+							minCol = j;
+						}
+					}
+				}
+				//decrease every considered row
+				//increase every forbidden col
+				Double x = minVal[minCol];
+				for(Long j = 0; j < m; j++){
+					if(forbidden[j]){
+						colAdd[j] += x;
+						rowAdd[matchCol[j]] -= x;
+					} else {
+						minVal[j] -= x;
+					}
+				}
+				rowAdd[i] -= x;
+				if(matchCol[minCol] == -1){
+					//solve conflicts
+					Long curRow = minPos[minCol];
+					Long oldMatch = matchRow[curRow];
+					matchCol[minCol] = curRow;
+					matchRow[curRow] = minCol;
+
+					while(curRow != i){
+						curRow = parent[curRow];
+						Long assigned = oldMatch;
+						oldMatch = matchRow[curRow];
+						matchCol[assigned]	= curRow;
+						matchRow[curRow] = assigned;
+					}
+					break;
+				} else {
+					forbidden[minCol] = true;
+					cur = matchCol[minCol]; //transition
+					parent[cur] = minPos[minCol]; //create conflict
+				}
+			}
+		}
+		
+		Double ans = 0;
+		for(Long i = 0; i < n; i++){
+			Long j = matchRow[i];
+			if(fabs(A[i][j] - dummy) > 1){
+				ans += A[i][j];
+			}
+		}
+		return ans;
+	}	
+}hg;
+   
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	Long n , m;
+	while(true){
+		cin >> m >> n;
+		if(n == 0 && m == 0) break;
+		Matrix M(n, vector<Double>(m));
+		REP(i , m){
+			REP(j , n){
+				cin >> M[j][i];
+			}
+		}
+		Double ans = hg.assign(M) / m;
+		ans = round(ans * 100) / 100 ;
+		cout << fixed << setprecision(2) << ans << endl;
+	}
+	
+	return 0;
+}
