c0660df0e3e7c03a5803cbcbad5d5ade7d6c4f5b
Diego <dhurtadodemendoza@gmail.com>
2020-08-20 12:07:46 -0500

    fix bug on fft

--- Math/Polynomials/FFT/Recursive/fft.cpp
+++ Math/Polynomials/FFT/Recursive/fft.cpp
@@ -59,7 +59,7 @@ polynomial operator *(const polynomial &a, const polynomial &b) {
 	fft(fa, true);
 	
 	polynomial ans(a.size() + b.size() - 1);
-	for(Long i = 0; i < n; i++){
+	for(Long i = 0; i < ans.size(); i++){
 		ans[i] = round(fa[i].real());
 	}
 	return ans;
