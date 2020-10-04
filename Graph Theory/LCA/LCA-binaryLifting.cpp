36250286f31b1da9b97d1645abe81260f3ed21c8
Diego <dhurtadodemendoza@gmail.com>
2020-10-03 23:48:11 -0500

    lca refactoring

--- Graph Theory/LCA/LCA-binaryLifting.cpp
+++ Graph Theory/LCA/LCA-binaryLifting.cpp
@@ -11,41 +11,41 @@ const Long loga = 32 - __builtin_clz(MX);
 
 struct Graph {
 	vector<int> adj[MX];
-	int anc[MX][loga]; //anc[i][j] : ancestor of i at distance 2^j
-	int height[MX];
+	int depth[MX];
 	int tIn[MX];
 	int tOut[MX];
-	int timer = 0;
+	int timer;
+	int anc[MX][loga];
 	
 	void clear(Long n){
 		for(Long i = 0; i < n; i++){
 			adj[i].clear();
 		}
-		timer = 0;
 	}
 	
-	void addEdge(int u , int v){
+	void addEdge(int u, int v) {
 		adj[u].push_back(v);
 		adj[v].push_back(u);
 	}
 	
-	void dfs(int u = 0){ //O(n+m)
+	void dfs(int u) {
 		tIn[u] = timer++;
-		for ( int v : adj[u] ) {
-			if(anc[u][0] != v){
+		for (int v : adj[u]) {
+			if (v != anc[u][0]) {
 				anc[v][0] = u;
-				height[v] = height[u] + 1;
+				depth[v] = depth[u] + 1;
 				dfs(v);
 			}
 		}
 		tOut[u] = timer++;
 	}
-
-	void precalculate(int n, int root = 0){ //O(nlogn)
+	
+	void precalculate(int n, int root = 0){ //O(n log n)
 		anc[root][0] = -1;
-		height[root] = 0;
+		depth[root] = 0;
+		timer = 0;
 		dfs(root);
-		for(int j = 1; (1 << j) < n; j++){
+		for (int j = 1; (1 << j) < n; j++){
 			for(int i = 0; i < n; i++){
 				if(anc[i][j-1] != -1){
 					anc[i][j] = anc[anc[i][j-1]][j-1];
@@ -56,31 +56,25 @@ struct Graph {
 		}
 	}
 	
-	int lca(int u, int v){ //O(logn)
-		if(height[u] < height[v]){
-			swap(u,v);
-		}
-		int lg = 31 - (__builtin_clz(height[u]));
-		for(int i = lg; i >= 0; i--){
-			if(height[u] - (1 << i) >= height[v]){
-				u = anc[u][i];
-			}
-		}
-		
-		if(u == v){
-			return u;
-		}
-		for(int i = lg; i >= 0; i--){
-			if(anc[u][i] != anc[v][i]){
-				u = anc[u][i], v = anc[v][i];
-			}
-		}
-		return anc[u][0];
-	}
-	
-	bool isAncestor(Long u, Long v){ //is u ancestor of v ?
+	bool isAncestor(int u, int v){ //is u ancestor of v ?
 		return tIn[u] <= tIn[v] && tOut[u] >= tOut[v];
 	}	
+	
+	int lca(int u, int v) { //O(log n)
+		/*if (depth[u] > depth[v]) {
+			swap(u , v);
+		}*/
+		if (isAncestor(u , v)) {
+			return u;
+		}
+		int bits = 31 - __builtin_clz(depth[u]);
+		for (int i = bits; i >= 0; i--) {
+			if (anc[u][i] != -1 && !isAncestor(anc[u][i] , v)) {
+				u = anc[u][i];
+			}
+		} 
+		return anc[u][0];
+	}
 	
 	bool onPath(int A, int B, int C) { //is C on AB path ?
 		int x = lca(A , B); 
