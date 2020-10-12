1db5940a354c26a4c63f6fc9dde1b36aaa29951b
Diego <dhurtadodemendoza@gmail.com>
2020-10-12 17:22:52 -0500

    formatting

--- DataStructure/MaxMinQueue/maxMinQueue.cpp
+++ DataStructure/MaxMinQueue/maxMinQueue.cpp
@@ -4,34 +4,33 @@ using namespace std;
 typedef long long Long;
 
 struct MaxQueue{
-	stack<pair<Long,Long>> s1,s2; // ( valor , maximum)
+	stack<pair<Long, Long>> s1, s2; // ( value , maximum)
 	
-	Long f(Long a, Long b){
-		return max(a,b);
+	Long f(Long a, Long b) {
+		return max(a, b);
 	}
 
-	Long get(){ //O(1) amortized
-		if(s1.empty() && s2.empty()) return 0;
-		if(s1.empty() || s2.empty()){
+	Long get() { //O(1) amortized
+		if (s1.empty() && s2.empty()) return 0;
+		if (s1.empty() || s2.empty()) {
 			return s1.empty() ? s2.top().second : s1.top().second;
-		}
-		else{
+		} else {
 			return f(s1.top().second, s2.top().second);
 		}
 	}
 	
 	void add(Long newElement){ //O(1) amortized
-		Long aux = s1.empty() ? newElement : f(newElement,s1.top().second);
+		Long aux = s1.empty() ? newElement : f(newElement, s1.top().second);
 		s1.push(make_pair(newElement,aux));
 	}
 	
 	void pop(){ //O(1) amortized
-		if(s2.empty()){
+		if (s2.empty()) {
 			//s1 = inverse(s2)
-			while(!s1.empty()){
+			while (!s1.empty()) {
 				Long element = s1.top().first;
 				s1.pop();
-				Long aux = s2.empty() ? element : f(element,s2.top().second);
+				Long aux = s2.empty() ? element : f(element, s2.top().second);
 				s2.push(make_pair(element,aux));
 			}
 		}
@@ -39,20 +38,16 @@ struct MaxQueue{
 		s2.pop();
 	}
 	
-	void clear(){
-		while(!s1.empty()){
+	void clear() {
+		while (!s1.empty()) {
 			s1.pop();
 		}
-		while(!s2.empty()){
+		while (!s2.empty()) {
 			s2.pop();
 		}
 	}
 }mq;
 
-
 int main() {
 	return 0;
 }
-
-
-
--- DataStructure/MaxMinStack/maxMinStack.cpp
+++ DataStructure/MaxMinStack/maxMinStack.cpp
@@ -4,37 +4,33 @@ using namespace std;
 typedef long long Long;
 
 struct MaxStack{
-	stack<pair<Long,Long>> s; // ( valor , maximum)
+	stack<pair<Long, Long>> s; // ( value , maximum)
 	
-	Long f(Long a, Long b){
-		return max(a,b);
+	Long f(Long a, Long b) {
+		return max(a, b);
 	}
 
-	Long get(){ //O(1) amortized
-		if(s.empty()) return 0;
+	Long get() { //O(1) amortized
+		if (s.empty()) return 0;
 		return s.top().second;
 	}
 	
-	void add(Long newElement){ //O(1) amortized
+	void add(Long newElement) { //O(1) amortized
 		Long newVal = s.empty() ? newElement : min(newElement, s.top().second);
 		s.push({newElement , newVal});
 	}
 	
-	void pop(){ //O(1) amortized
+	void pop() { //O(1) amortized
 		s.pop();
 	}
 	
-	void clear(){
-		while(!s.empty()){
+	void clear() {
+		while (!s.empty()) {
 			s.pop();
 		}
 	}
 }ms;
 
-
 int main() {
 	return 0;
 }
-
-
-
