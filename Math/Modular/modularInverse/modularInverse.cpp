0c66c813f2fe6dfff3a1400143951495a8d85a0c
Diego <dhurtadodemendoza@gmail.com>
2020-10-19 00:25:54 -0500

    flow notes update. Modular Inverse MOD

--- Math/Modular/modularInverse/modularInverse.cpp
+++ Math/Modular/modularInverse/modularInverse.cpp
@@ -6,29 +6,35 @@ using namespace std;
 
 typedef long long Long;
 
-Long mult(Long a, Long b, Long mod){
-	return (a * b ) % mod;
+const Long MOD = 1e9 + 7;
+
+Long mult(Long a, Long b) {
+	return (a * b ) % MOD;
 }
 
-Long fastPow(Long a, Long b , Long mod){ //O(logb)
+Long fastPow(Long a, Long b) { //O(logb)
 	Long ans = 1;
-	while(b > 0){
-		if(b & 1 == 1){ //b % 2 == 1
-			ans = mult(ans ,a , mod);
+	while (b > 0) {
+		if (b & 1 == 1) { 
+			ans = mult(ans , a);
 		}
-		a = mult(a , a  , mod);
-		b >>= 1; //b /= 2;
+		a = mult(a , a);
+		b >>= 1; 
 	}
 	return ans;
 }
 
-/*Long modInverse(Long a, Long mod){ //O(log mod) , mod prime , (a , mod) coprimes
-	return fastPow(a,mod - 2,mod);
-}*/
+Long modInverse(Long a) { //O(log mod) , mod prime , (a , mod) coprimes
+	return fastPow(a, MOD - 2);
+}
 
-Long gcd(Long a, Long b, Long & x, Long & y) { //O(min(loga,logb))
-	if(b == 0){
-		if(a >= 0) {
+Long divide(Long a, Long b) {
+	return mult(a, modInverse(b));
+}
+
+Long gcd(Long a, Long b, Long &x, Long &y) { //O(min(loga,logb))
+	if (b == 0){
+		if (a >= 0) {
 			x = 1;
 		} else {
 			x = -1;
@@ -37,23 +43,22 @@ Long gcd(Long a, Long b, Long & x, Long & y) { //O(min(loga,logb))
 		return abs(a);
 	}
 	Long x2, y2;
-	Long g = gcd(b, a % b , x2, y2);
+	Long g = gcd(b, a % b, x2, y2);
 	x = y2;
 	y = x2 - (a / b) * y2;
 	return g;
 }
 
-Long modInverse(Long a, Long mod){ //O(min(log a,log mod))
+Long modInverseGCD(Long a) { //O(min(log a,log mod))
 	//a , mod coprime
-	Long x,y;
-	Long g = gcd(a, mod ,x,y);
+	Long x, y;
+	Long g = gcd(a, MOD ,x, y);
 	
-	if(g != 1) return -1;
-	if(x >= 0) return x;
-	else return x + mod;
+	if (g != 1) return -1;
+	if (x >= 0) return x;
+	else return x + MOD;
 }
 
 int main() {
-	Long mod = 1e9+7;
 	return 0;
 }
