33b7bc3f992a81c20fc48507d7e57d2147edbd0a
Diego <dhurtadodemendoza@gmail.com>
2020-10-28 12:38:37 -0500

    minor changes

--- Graph Theory/bfs/bfs.cpp
+++ Graph Theory/bfs/bfs.cpp
@@ -14,38 +14,37 @@ struct Graph{
 	Long d[MX];
 	vector<Long> adj[MX];
 	
-	void clear(Long N = MX) {
-		REP(i , N) {
+	void clear(Long n) {
+		for (int i = 0; i < n; i++) {
 			adj[i].clear();
 		}
 	}
 	
 	void addEdge(Long u, Long v) {
-		adj[u].pb(v);
-		adj[v].pb(u);
+		adj[u].push_back(v);
+		adj[v].push_back(u);
 	}
 	
-	void bfs(Long s, Long n){ //O(n+m)
-		for(Long i = 0; i < n; i++){
+	void bfs(Long s, Long n) { //O(n + m)
+		for (Long i = 0; i < n; i++) {
 			d[i] = INF;
 		}
 		d[s] = 0;
 		deque<Long> Q;
 		Q.push_back(s);
 		
-		while(!Q.empty()){
+		while (!Q.empty()) {
 			Long u = Q.front();
 			Q.pop_front();
 			
-			for (Long v : adj[u] ) {
-				if(d[v] == INF){
+			for (Long v : adj[u]) {
+				if (d[v] == INF) {
 					d[v] = d[u] + 1;
 					Q.push_back(v);
 				}
 			}
 		}
 	}
-
 }G;
 
 int main() {
