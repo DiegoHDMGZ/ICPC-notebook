efbed2d40d0171336c08f60883667a4bc0c4a183
Diego <dhurtadodemendoza@gmail.com>
2020-11-03 23:15:47 -0500

    change in template

--- template.cpp
+++ template.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 #define debug(x) cout << #x << " = " << x << endl
-#define REP(i,n) for(Long i = 0; i < (Long)n; i++)
-#define pb push_back
+#define REP(i, n) for(Long i = 0; i < (Long)n; i++)
 using namespace std;
 
 typedef long long Long;
