abdba341736e15bb8af6e0cea4970069f4cac8e2
Diego <dhurtadodemendoza@gmail.com>
2020-11-03 19:16:58 -0500

    minor change

--- Util/parsing.cpp
+++ Util/parsing.cpp
@@ -1,24 +1,18 @@
 #include <bits/stdc++.h>
-#define debug(x) cout << #x << " = " << x << endl
-#define REP(i,n) for(Long i = 0; i < (Long)n; i++)
-#define pb push_back
 using namespace std;
 
 typedef long long Long;
 
-vector<Long> parsing( string &s ){
+vector<Long> parsing(string &s) {
 	Long tokens;  
-	stringstream in( s );
+	stringstream in(s);
 	vector<Long> out;
-	while( in >> tokens ){
+	while (in >> tokens) {
 		out.push_back(tokens);
 	}
 	return out;
 }
 
 int main() {
-
 	return 0;
 }
-
-
