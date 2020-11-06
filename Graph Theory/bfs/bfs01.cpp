c9abbfde0bb016ec2e3c7048d55109e2d276bdff
Diego <dhurtadodemendoza@gmail.com>
2020-11-06 13:45:58 -0500

    bfs01 simplification

--- Graph Theory/bfs/bfs01.cpp
+++ Graph Theory/bfs/bfs01.cpp
@@ -1,7 +1,4 @@
 #include <bits/stdc++.h>
-#define debug(x) cout << #x << " = " << x << endl
-#define REP(i,n) for(Long i = 0; i < (Long)n; i++)
-#define pb push_back
 using namespace std;
 
 typedef long long Long;
@@ -9,53 +6,42 @@ typedef long long Long;
 const Long MX = 1e5;
 const Long INF = 1e18;
 
-struct EndPoint{
-	Long node, w;
-	EndPoint(){}
-	EndPoint(Long node, Long w) : node(node), w(w) {}
-};
-
 struct Graph{
 	Long d[MX];
-	bool used[MX];
-	vector<EndPoint> adj[MX];
+	vector<pair<Long, Long>> adj[MX];
 	
-	void clear(Long N = MX) {
-		REP(i, N) {
+	void clear(Long n) {
+		for (Long i = 0; i < n; i++) {
 			adj[i].clear();
 		}
 	}
 	
 	void addEdge(Long u , Long v, Long w) {
-		adj[u].pb(EndPoint(v , w));
-		adj[v].pb(EndPoint(u , w));
+		adj[u].push_back({v, w});
+		adj[v].push_back({u, w});
 	}
 	
 	void bfs01(Long s, Long n){ //O(V + E)
 		for(Long i = 0; i < n; i++){
 			d[i] = INF;
-			used[i] = false;
 		}
 		d[s] = 0;
-		deque<Long> Q;
-		Q.pb(s);
+		deque<Long> q;
+		q.push_back(s);
 		
-		while(!Q.empty()){
-			Long u = Q.front();
-			Q.pop_front();
-			if(used[u]) {
-				continue;
-			}
-			used[u] = true;
-			for (EndPoint e : adj[u] ) {
-				Long v = e.node;
-				if(d[u] + e.w < d[v]) {
-					d[v] = d[u] + e.w;
-					if(e.w == 0){
-						Q.push_front(v);
+		while(!q.empty()){
+			Long u = q.front();
+			q.pop_front();
+			for (auto e : adj[u] ) {
+				Long v = e.first;
+				Long w = e.second;
+				if(d[u] + w < d[v]) {
+					d[v] = d[u] + w;
+					if(w == 0){
+						q.push_front(v);
 					}
 					else{
-						Q.push_back(v);
+						q.push_back(v);
 					}
 				}
 			}
