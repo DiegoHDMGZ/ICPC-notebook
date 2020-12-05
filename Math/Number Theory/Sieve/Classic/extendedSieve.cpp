abc2504513eeb02f7b20be32d6d8633a405568f2
Diego <dhurtadodemendoza@gmail.com>
2020-12-05 10:23:35 -0500

    minor changes in extended sieve

--- Math/Number Theory/Sieve/Classic/extendedSieve.cpp
+++ Math/Number Theory/Sieve/Classic/extendedSieve.cpp
@@ -10,17 +10,15 @@ bool isPrime[MX];
 Long fact[MX];
 vector<Long> primes;
 
-//pending system test
-
-void sieve(){ //O(MX log log MX)
+void sieve() { //O(MX log log MX)
 	fill(isPrime, isPrime + MX , true);
 	isPrime[0] = isPrime[1] = false;
-	for(Long i = 2; i  < MX; i++){
-		if(isPrime[i]) {
-			primes.pb(i);
+	for (Long i = 2; i  < MX; i++) {
+		if (isPrime[i]) {
+			primes.push_back(i);
 			fact[i] = i;
-			if(i * i < MX){
-				for(Long j = i * i; j < MX; j += i){
+			if (i * i < MX) {
+				for (Long j = i * i; j < MX; j += i) {
 					isPrime[j] = false;
 					fact[j] = i;
 				}
@@ -29,49 +27,38 @@ void sieve(){ //O(MX log log MX)
 	}
 }
 
-struct Factor{
-	Long base,exp;
-	Factor(){
-	}
-	Factor(Long b, Long e){
-		base = b;
-		exp = e;
-	}
-};
-deque<Factor> factores;
-
-void factorize(Long x){ //O(log x)
-	factores.clear();
-	while(x > 1){
+vector<pair<Long,Long>> factorize(Long x) { //O(log x)
+	vector<pair<Long,Long>> factors;
+	while (x > 1) {
 		Long f = fact[x];
 		Long exp = 0;
-		while(x % f == 0){
+		while (x % f == 0) {
 			x /= f;
 			exp++;
 		}
-		factores.push_front(Factor(f,exp));
+		factors.push_back({f ,exp});
 	}
-	
+	//not necessarily sorted
+	return factors;
 }
 
 vector<Long> getDivisors(Long x) {
 	vector<Long> ans = {1};
-	while(x > 1) {
+	while (x > 1) {
 		Long f = fact[x];
 		Long num = 1;
 		Long sz = ans.size();
-		while(x % f == 0) {
+		while (x % f == 0) {
 			num *= f;
 			x /= f;
-			for(Long i = 0; i < sz; i++) {
-				ans.pb(num * ans[i]);
+			for (Long i = 0; i < sz; i++) {
+				ans.push_back(num * ans[i]);
 			}
 		}
 	}
 	//sort(ans.begin(), ans.end());
 	return ans;
 }
-
 
 int main() {
 	sieve();
--- Math/Number Theory/Sieve/Optimized/extendedSieve.cpp
+++ Math/Number Theory/Sieve/Optimized/extendedSieve.cpp
@@ -11,59 +11,50 @@ bool isPrime[MX];
 Long fact[MX];
 vector<Long> primes;
 
-void sieve(){ //O(MX)
+void sieve() { //O(MX)
 	//x = i * p. p is the smallest prime factor
 	fill(isPrime, isPrime + MX , true);
 	isPrime[1] = false;
-	for(Long i = 2; i  < MX; i++){
-		if(isPrime[i]) {
+	for (Long i = 2; i  < MX; i++) {
+		if (isPrime[i]) {
 			primes.push_back(i);
 			fact[i] = i;
 		}
-		for(Long j = 0 ; j < primes.size() && i * primes[j] < MX; j++){
+		for (Long j = 0 ; j < primes.size() && i * primes[j] < MX; j++) {
 			isPrime[i * primes[j]] = false;
 			fact[i * primes[j]] = primes[j];
-			if(i % primes[j] == 0) {
+			if (i % primes[j] == 0) {
 				break;
 			}
 		}
 	}
 }
 
-struct Factor{
-	Long base,exp;
-	Factor(){
-	}
-	Factor(Long b, Long e){
-		base = b;
-		exp = e;
-	}
-};
-
-vector<Factor> factorize(Long x){ //O(log x)
-	vector<Factor> factors;
-	while(x > 1){
+vector<pair<Long,Long>> factorize(Long x) { //O(log x)
+	vector<pair<Long,Long>> factors;
+	while (x > 1) {
 		Long f = fact[x];
 		Long exp = 0;
-		while(x % f == 0){
+		while (x % f == 0) {
 			x /= f;
 			exp++;
 		}
-		factors.push_back(Factor(f,exp));
+		factors.push_back({f ,exp});
 	}
+	//sorted factors
 	return factors;
 }
 
 vector<Long> getDivisors(Long x) {
 	vector<Long> ans = {1};
-	while(x > 1) {
+	while (x > 1) {
 		Long f = fact[x];
 		Long num = 1;
 		Long sz = ans.size();
-		while(x % f == 0) {
+		while (x % f == 0) {
 			num *= f;
 			x /= f;
-			for(Long i = 0; i < sz; i++) {
+			for (Long i = 0; i < sz; i++) {
 				ans.push_back(num * ans[i]);
 			}
 		}
@@ -71,8 +62,6 @@ vector<Long> getDivisors(Long x) {
 	//sort(ans.begin(), ans.end());
 	return ans;
 }
-
-
 
 int main() {
 	sieve();
