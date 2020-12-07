b270d419fe1c0e2a04f51ab313cb90dd6cf2c589
Diego <dhurtadodemendoza@gmail.com>
2020-12-06 21:57:34 -0500

    minor changes

--- Strings/hashing/hashing.cpp
+++ Strings/hashing/hashing.cpp
@@ -30,47 +30,46 @@ struct Hashing{
 	Long hPref[MX];
 	Long hSuf[MX];
 	
-	Hashing(){
+	Hashing() { 
 		MOD = 1e9 + 7; //1e9 + 1269
 		B = 67;
 	}
 	
-	void setData(Long b, Long mod){
+	void setData(Long b, Long mod) {
 		B = b;
 		MOD = mod;
 	}
 	
-	Long hSub(Long i, Long j){ //O(1)
+	Long hSub(Long i, Long j) { //O(1)
 		if(i == 0) return hPref[j];
 		return sub(hPref[j] , mult(hPref[i - 1] , pot[j - i + 1], MOD) , MOD);
 	}
 
-	Long hSub2(Long i, Long j){ //O(1)
+	Long hSub2(Long i, Long j) { //O(1)
 		return sub(hSuf[i] , mult(hSuf[j + 1] , pot[j - i + 1], MOD) , MOD);
 	}
 
 
-	bool isPalindrome(Long i, Long j){ //O(1)
+	bool isPalindrome(Long i, Long j) { //O(1)
 		return hSub(i , j) == hSub2(i , j);
 	}
 
-	void precalc(){
+	void precalc() {
 		pot[0]= 1;
-		for(int i = 1; i<MX;i++){
+		for (int i = 1; i< MX;i++) {
 			pot[i] = mult(pot[i - 1] , B , MOD);
 		}
 	}
 
-	void precalc(string &s){ //O(size)
+	void precalc(string &s) { //O(size)
 		Long sz = s.size();
 		hPref[0] = s[0] - minChar + 1;
-		for(int i = 1; i < s.size(); i++){
+		for (int i = 1; i < sz; i++) {
 			hPref[i] = add( mult(hPref[i - 1] , B, MOD) , s[i] - minChar + 1, MOD);
 		}
-		
 		hSuf[sz] = 0;
 		hSuf[sz - 1] = s[sz - 1]- minChar + 1;
-		for(int i = sz - 2; i >= 0; i--){
+		for (int i = sz - 2; i >= 0; i--) {
 			hSuf[i] = add(mult( hSuf[i + 1] , B, MOD) ,  s[i] - minChar + 1 , MOD);
 		}
 	}
