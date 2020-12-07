a9c8b39b1ed338e9f735bfe7b8758566b63b8223
Diego <dhurtadodemendoza@gmail.com>
2020-12-07 00:42:45 -0500

    multiple hashes

--- Strings/hashing/hashing.cpp
+++ Strings/hashing/hashing.cpp
@@ -21,57 +21,77 @@ Long sub(Long a , Long b, Long mod){
 
 Long minChar = (Long)'0';
 
-const Long MX = 1e6;
-struct Hashing{
-	Long MOD;
-	Long BASE;
-	Long pot[MX];
-	Long hPref[MX];
-	Long hSuf[MX];
+struct Hash{
+	Long mod, base;
+	vector<Long> pot, hashPref, hashSuf;
 	
-	Hashing() { 
-		MOD = 1e9 + 7; //1e9 + 1269
-		BASE = 67;
-	}
-	
-	void setData(Long b, Long mod) {
-		BASE = b;
-		MOD = mod;
-	}
-	
-	Long hash(Long i, Long j) { //O(1)
-		if(i == 0) return hPref[j];
-		return sub(hPref[j] , mult(hPref[i - 1] , pot[j - i + 1], MOD) , MOD);
-	}
-
-	Long hashInverse(Long i, Long j) { //O(1)
-		return sub(hSuf[i] , mult(hSuf[j + 1] , pot[j - i + 1], MOD) , MOD);
-	}
-	
-	bool isPalindrome(Long i, Long j) { //O(1)
-		return hash(i , j) == hashInverse(i , j);
-	}
-
-	void precalc() {
+	Hash(Long n, Long mod = 1e9 + 7 , Long base = 67) {
+		//other mod 1e9 + 1269
+		this->mod = mod;
+		this->base = base;
+		pot = hashPref = hashSuf = vector<Long>(n);
 		pot[0] = 1;
-		for (int i = 1; i < MX; i++) {
-			pot[i] = mult(pot[i - 1] , BASE , MOD);
+		for (int i = 1; i < n; i++) {
+			pot[i] = mult(pot[i - 1] , base , mod);
 		}
+	}
+	
+	Long hash(Long l, Long r) { //O(1)
+		if(l == 0) return hashPref[r];
+		return sub(hashPref[r] , mult(hashPref[l - 1] , pot[r - l + 1], mod) , mod);
 	}
 
-	void precalc(string &s) { //O(size)
+	Long hashInverse(Long l, Long r) { //O(1)
+		return sub(hashSuf[l], mult(hashSuf[r + 1], pot[r - l + 1], mod) , mod);
+	}
+	
+	bool isPalindrome(Long l, Long r) { //O(1)
+		return hash(l , r) == hashInverse(l , r);
+	}
+
+	void precalc(string &s) { //O(n)
 		Long sz = s.size();
-		hPref[0] = s[0] - minChar + 1;
+		hashPref[0] = s[0] - minChar + 1;
 		for (int i = 1; i < sz; i++) {
-			hPref[i] = add( mult(hPref[i - 1] , BASE, MOD) , s[i] - minChar + 1, MOD);
+			hashPref[i] = add(mult(hashPref[i - 1], base, mod), s[i] - minChar + 1, mod);
 		}
-		hSuf[sz] = 0;
-		hSuf[sz - 1] = s[sz - 1]- minChar + 1;
+		hashSuf[sz] = 0;
+		hashSuf[sz - 1] = s[sz - 1]- minChar + 1;
 		for (int i = sz - 2; i >= 0; i--) {
-			hSuf[i] = add(mult( hSuf[i + 1] , BASE, MOD) ,  s[i] - minChar + 1 , MOD);
+			hashSuf[i] = add(mult(hashSuf[i + 1] , base, mod),  s[i] - minChar + 1, mod);
 		}
 	}
-}hs;
+};
+
+mt19937_64  rng(chrono::steady_clock::now().time_since_epoch().count());
+
+Long random(Long a, Long b) {
+	return uniform_int_distribution<Long>(a , b)(rng);
+}
+
+struct MultipleHash{
+	vector<Hash> hashes;
+	
+	MultipleHash(Long n , vector<Long> mods) {
+		for (Long m : mods) {
+			hashes.push_back(Hash(n, m, 2 * random(33, m / 2) + 1));
+		}
+	}
+	
+	void precalc(string &s) {
+		for (int i = 0; i < hashes.size(); i++) {
+			hashes[i].precalc(s);
+		}
+	}
+	
+	vector<Long> hash(Long l, Long r) {
+		vector<Long> ans;
+		for (Long i = 0; i < hashes.size(); i++) {
+			ans.push_back(hashes[i].hash(l, r));
+		}
+		return ans;
+	}
+};
  
 int main(){
 	ios_base::sync_with_stdio(NULL);
