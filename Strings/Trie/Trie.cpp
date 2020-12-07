d11276f636ff365e10b0f1da8c46f7840e3cc699
Diego <dhurtadodemendoza@gmail.com>
2020-12-06 22:54:01 -0500

    minor changes in trie

--- Strings/Trie/Trie.cpp
+++ Strings/Trie/Trie.cpp
@@ -14,22 +14,22 @@ struct Trie{
 	bool term[MX]; //terminal node
 	Long nodes = 1;
 	
-	void init(){
-		for(Long i = 0; i < nodes; i++){
+	void init() {
+		for (Long i = 0; i < nodes; i++) {
 			term[i] = 0;
-			for(int j = 0; j < ALPH; j++){
+			for (int j = 0; j < ALPH; j++) {
 				memo[i][j] = 0;
 			}
 		}
 		nodes = 1;
 	}
 	
-	void addWord(string &s){ //O(n)
+	void addWord(string &s) { //O(n)
 		int n = s.size();
 		int u = 0;
-		for(Long i = 0; i < n; i++){
+		for (Long i = 0; i < n; i++) {
 			Long c = s[i] - 'a';
-			if(memo[u][c] == 0){
+			if (memo[u][c] == 0) {
 				memo[u][c] = nodes++;
 			}
 			u = memo[u][c];
@@ -37,12 +37,12 @@ struct Trie{
 		term[u] = true;
 	}
 	
-	bool isPrefix(string &s){ //O(n)
+	bool isPrefix(string &s) { //O(n)
 		Long n = s.size();
 		Long u = 0;
-		for(Long i = 0; i < n; i++){
+		for (Long i = 0; i < n; i++) {
 			Long c = s[i] - 'a';
-			if(memo[u][c] == 0){
+			if (memo[u][c] == 0) {
 				return false;
 			}
 			u = memo[u][c];
@@ -50,36 +50,21 @@ struct Trie{
 		return true;
 	}
 	
-	bool isWord(string &s){ //O(n)
+	bool isWord(string &s) { //O(n)
 		Long n = s.size();
 		Long u = 0;
 		
-		for(Long i = 0; i < n; i++){
+		for (Long i = 0; i < n; i++) {
 			Long c = s[i] - 'a';
-			if(memo[u][c] == 0){
+			if (memo[u][c] == 0) {
 				return false;
 			}
 			u = memo[u][c];
 		}
 		return term[u]; 
 	}
-		
 }trie;
 
-
-
 int main() {
-	string A[] = {"pikachu", "mama", "mar","marco", "margarita", "pikachu"};
-	int n = 6;
-	for(Long i = 0; i < n; i++) trie.addWord(A[i]);
-	while(true){
-		string s;
-		cin >> s;
-		cout << "Is Prefix : " << trie.isPrefix(s) << endl;
-		cout << "Is Word : " << trie.isWord(s) << endl;
-	}
-	
 	return 0;
 }
-
-
