6e4a5f987153be334e8540183e9b5bd496a6322a
Diego <dhurtadodemendoza@gmail.com>
2020-12-09 11:48:21 -0500

    change variable names

--- Strings/KMP/KMP.cpp
+++ Strings/KMP/KMP.cpp
@@ -6,45 +6,38 @@ using namespace std;
 
 typedef long long Long;
 
-const Long MX = 1e5;
-vector<Long> b(MX); //b[i] es la longitud del borde más grande en la subcadena s[0...i]
-
-void prefixFunction(string &s){ //O(n)
+vector<Long> prefixFunction(string &s) { //O(n)
 	Long n = s.size();
-	
-	for(Long i = 1; i < n; i++){
-		Long bordeAnterior = b[i-1];
-		while(bordeAnterior >0 && s[i] != s[bordeAnterior]/*el que le sigue al borde*/ ){
-			bordeAnterior = b[bordeAnterior-1];
+	vector<Long> b(n);
+	//b[i] is the lenght of the longest proper preffix in s[0...i] that is also a suffix
+	for (Long i = 1; i < n; i++) {
+		Long lastBorder = b[i - 1];
+		while (lastBorder > 0 && s[i] != s[lastBorder]) {
+			lastBorder = b[lastBorder - 1];
 		}
-		
-		if(s[i] == s[bordeAnterior]){
-			bordeAnterior++;
+		if (s[i] == s[lastBorder]) {
+			lastBorder++;
 		}
-		
-		b[i] = bordeAnterior;
+		b[i] = lastBorder;
 	}
+	return b;
 }
 
-vector<Long> ans; //posiciones de la cadena en donde empieza una ocurrencia de un patrón
-
-void kmp(string &p, string &t){ //O(n+m)
-//determina si el patron p aparece en t y enumera las posiciones de ocurrencia
+vector<Long> kmp(string &p, string &t) { //O(|p| + |t|)
+	//return the position where start an ocurrence of pattern p in t
 	Long n = p.size();
 	Long m = t.size();
-	ans.clear();
 	string s = p + "#" + t;
-	
-	prefixFunction(s);
-	
-	for(Long i = 1; i < (Long)s.size(); i++){
-		if(b[i] == n){
+	vector<Long> b = prefixFunction(s);
+	vector<Long> ans;
+	for (Long i = 1; i < (Long)s.size(); i++) {
+		if (b[i] == n) {
 			ans.push_back(i - (n + 1) - n + 1);
 		}
 	}
+	return ans;
 }
 
 int main(){
-
 	return 0;
 }
