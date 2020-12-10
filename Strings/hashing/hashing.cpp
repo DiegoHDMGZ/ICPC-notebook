78b5f22cb2f56ec9915e2179529c2b266d78ffbd
Diego <dhurtadodemendoza@gmail.com>
2020-12-09 20:42:44 -0500

    refactoring hash

--- Strings/hashing/hashing.cpp
+++ Strings/hashing/hashing.cpp
@@ -25,15 +25,10 @@ struct Hash{
 	Long mod, base;
 	vector<Long> pot, hashPref, hashSuf;
 	
-	Hash(Long n, Long mod = 1e9 + 7 , Long base = 67) {
+	Hash(Long mod = 1e9 + 7 , Long base = 67) {
 		//other mod 1e9 + 1269
 		this->mod = mod;
 		this->base = base;
-		pot = hashPref = hashSuf = vector<Long>(n);
-		pot[0] = 1;
-		for (int i = 1; i < n; i++) {
-			pot[i] = mult(pot[i - 1] , base , mod);
-		}
 	}
 	
 	Long hash(Long l, Long r) { //O(1)
@@ -50,31 +45,29 @@ struct Hash{
 	}
 
 	void precalc(string &s) { //O(n)
-		Long sz = s.size();
+		Long n = s.size();
+		pot = hashPref = vector<Long>(n);
+		hashSuf = vector<Long>(n + 1);
+		pot[0] = 1;
 		hashPref[0] = s[0] - minChar + 1;
-		for (int i = 1; i < sz; i++) {
+		for (int i = 1; i < n; i++) {
 			hashPref[i] = add(mult(hashPref[i - 1], base, mod), s[i] - minChar + 1, mod);
+			pot[i] = mult(pot[i - 1] , base , mod);
 		}
-		hashSuf[sz] = 0;
-		hashSuf[sz - 1] = s[sz - 1]- minChar + 1;
-		for (int i = sz - 2; i >= 0; i--) {
+		hashSuf[n] = 0;
+		hashSuf[n - 1] = s[n - 1] - minChar + 1;
+		for (int i = n - 2; i >= 0; i--) {
 			hashSuf[i] = add(mult(hashSuf[i + 1] , base, mod),  s[i] - minChar + 1, mod);
 		}
 	}
 };
 
-mt19937_64  rng(chrono::steady_clock::now().time_since_epoch().count());
-
-Long random(Long a, Long b) {
-	return uniform_int_distribution<Long>(a , b)(rng);
-}
-
-struct MultipleHash{
+struct MultiHash{
 	vector<Hash> hashes;
 	
-	MultipleHash(Long n , vector<Long> mods) {
-		for (Long m : mods) {
-			hashes.push_back(Hash(n, m, 2 * random(33, m / 2) + 1));
+	MultiHash(vector<Long> mods, vector<Long> bases) {
+		for (Long i = 0; i < mods.size(); i++) {
+			hashes.push_back(Hash(mods[i], bases[i]));
 		}
 	}
 	
@@ -91,7 +84,31 @@ struct MultipleHash{
 		}
 		return ans;
 	}
+	
+	bool isPalindrome(Long l, Long r) {
+		for (Long i = 0; i < hashes.size(); i++) {
+			if (!hashes[i].isPalindrome(l , r)) {
+				return false;
+			}
+		}
+		return true;
+	}
 };
+
+mt19937_64  rng(chrono::steady_clock::now().time_since_epoch().count());
+
+Long random(Long a, Long b) {
+	return uniform_int_distribution<Long>(a , b)(rng);
+}
+
+vector<Long> getBases(vector<Long> mods) {
+	vector<Long> ans;
+	for (Long m : mods) {
+		Long base = 2 * random(33, m / 2) + 1; //odd base
+		ans.push_back(base);
+	}
+	return ans;
+}
  
 int main(){
 	return 0;
--- Strings/hashing/hashing2D.cpp
+++ Strings/hashing/hashing2D.cpp
@@ -21,74 +21,98 @@ Long sub(Long a , Long b, Long mod){
 
 Long minChar = (Long)'0';
 
-const Long MX = 1e3;
-
-typedef vector<vector<char>> Matrix;
-struct Hashing{
-	Long MOD;
-	Long B;
- 
-	int pot[MX * MX];
-	int hPref[MX][MX];
+const int MX = 1000;
+struct Hash{
+	Long mod, base;
+	vector<Long> pot;
+	vector<vector<Long>> hashPref;
 	
-	Hashing(){
-		MOD = 1e9 + 7; //1e9 + 1269
-		B = 67;
+	Hash(Long mod = 1e9 + 7 , Long base = 67) {
+		//other mod 1e9 + 1269
+		this->mod = mod;
+		this->base = base;
+		hashPref = vector<vector<Long>>(MX, vector<Long>(MX));
+		pot = vector<Long>(MX * MX);
+		pot[0] = 1;
+		for (int i = 1; i < MX * MX; i++) {
+			pot[i] = mult(pot[i - 1] , base , mod);
+		}
 	}
 	
-	void setData(Long b, Long mod){
-		B = b;
-		MOD = mod;
-	}
-	
-	Long hSub(Long x, Long y){ //O(1)
+	Long hash(Long x, Long y){ //O(1)
 		if(x < 0 || y < 0) return 0;
-		return hPref[x][y];
+		return hashPref[x][y];
 	}
 	
-	Long hSub(Long x1, Long y1, Long x2 , Long y2){ //O(1)
-		Long ans = hSub(x2 , y2);
-		ans = sub(ans , mult(hSub(x2, y1 - 1) , pot[y2 - y1 + 1] , MOD)  , MOD); 
-		ans = sub(ans , mult( hSub(x1 - 1 , y2) , pot[MX * (x2 - x1 + 1)], MOD) , MOD  );
-		ans = add(ans , mult( hSub(x1 - 1 , y1 - 1) , pot[MX * (x2 - x1 + 1) + (y2 - y1 + 1)] , MOD) , MOD);
+	Long hash(Long x1, Long y1, Long x2 , Long y2){ //O(1)
+		Long sz = hashPref.size();
+		Long ans = hash(x2 , y2);
+		ans = sub(ans , mult(hash(x2, y1 - 1) , pot[y2 - y1 + 1] , mod)  , mod); 
+		ans = sub(ans , mult(hash(x1 - 1 , y2) , pot[sz * (x2 - x1 + 1)], mod) , mod  );
+		ans = add(ans , mult(hash(x1 - 1 , y1 - 1) , pot[sz * (x2 - x1 + 1) + (y2 - y1 + 1)] , mod) , mod);
 		return ans;
 	}
  
-	void precalc(){ //O(MX * MX)
-		pot[0]= 1;
-		for(int i = 1; i<MX * MX;i++){
-			pot[i] = mult(pot[i - 1] , B , MOD);
-		}
-	}
- 
-	void precalc(Matrix &A){ //O(n * m)
+	void precalc(vector<vector<char>> &A){ //O(n * m)
 		Long n = A.size();
 		Long m = A[0].size();
-		
-		REP(i , n) {
-			REP(j , m){
-				
-				hPref[i][j] = A[i][j] - minChar + 1;
-				
+		Long sz = hashPref.size();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++){
+				hashPref[i][j] = A[i][j] - minChar + 1;
 				if(i > 0) {
-					hPref[i][j] = add(hPref[i][j] , mult(hPref[i - 1][j] , pot[MX] , MOD) , MOD);
+					hashPref[i][j] = add(hashPref[i][j] , mult(hashPref[i - 1][j] , pot[sz] , mod) , mod);
 				}
 				if(j > 0){
-					hPref[i][j] = add(hPref[i][j] , mult(hPref[i][j - 1] , B, MOD) , MOD);
+					hashPref[i][j] = add(hashPref[i][j] , mult(hashPref[i][j - 1] , base, mod) , mod);
 				}
 				if(i > 0 && j > 0){
-					hPref[i][j] = sub(hPref[i][j] , mult(mult(B , pot[MX] , MOD), hPref[i - 1][j - 1] , MOD) , MOD);
+					hashPref[i][j] = sub(hashPref[i][j] , mult(mult(base , pot[sz] , mod), hashPref[i - 1][j - 1] , mod) , mod);
 				}
 			}
 		}
 	}
-}hs;
+};
+
+struct MultiHash{
+	vector<Hash> hashes;
+	
+	MultiHash(vector<Long> mods, vector<Long> bases) {
+		for (Long i = 0; i < mods.size(); i++) {
+			hashes.push_back(Hash(mods[i], bases[i]));
+		}
+	}
+	
+	void precalc(vector<vector<char>> &s) {
+		for (int i = 0; i < hashes.size(); i++) {
+			hashes[i].precalc(s);
+		}
+	}
+	
+	vector<Long> hash(Long x1, Long y1, Long x2, Long y2) {
+		vector<Long> ans;
+		for (Long i = 0; i < hashes.size(); i++) {
+			ans.push_back(hashes[i].hash(x1, y1, x2, y2));
+		}
+		return ans;
+	}
+};
+
+mt19937_64  rng(chrono::steady_clock::now().time_since_epoch().count());
+
+Long random(Long a, Long b) {
+	return uniform_int_distribution<Long>(a , b)(rng);
+}
+
+vector<Long> getBases(vector<Long> mods) {
+	vector<Long> ans;
+	for (Long m : mods) {
+		Long base = 2 * random(33, m / 2) + 1; //odd base
+		ans.push_back(base);
+	}
+	return ans;
+}
  
 int main(){
-	ios_base::sync_with_stdio(NULL);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	
 	return 0;
 }
