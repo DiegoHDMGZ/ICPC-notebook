9cd57f959637422075257b47273588bac0865a2b
Diego <dhurtadodemendoza@gmail.com>
2020-12-23 13:07:24 -0500

    minor change in sparse table

--- DataStructure/SparseTable/sparseTable.cpp
+++ DataStructure/SparseTable/sparseTable.cpp
@@ -8,35 +8,35 @@ using namespace std;
 typedef long long Long;
 
 const Long MX = 1e6;
-const Long loga = 32 - __builtin_clz(MX);
+const Long LG = 32 - __builtin_clz(MX);
 
-struct SparseTable{
-	Long st[MX][loga];
+struct SparseTable {
+	Long st[MX][LG];
 	
-	Long f(Long a, Long b){
+	Long f(Long a, Long b) {
 		return min(a , b);
 	}
 
-	void build(vector<Long> &A){ // O(|f| n log n)
+	void build(vector<Long> &A) { // O(|f| n log n)
 		Long n = A.size();
-		for(Long i = 0; i < n; i++){
+		for (Long i = 0; i < n; i++) {
 			st[i][0] = A[i];
 		}
 		
-		for(Long k = 1; (1 << k) <= n; k++){
-			for(Long i = 0; i + (1 << k) <= n; i++){
+		for (Long k = 1; (1 << k) <= n; k++) {
+			for (Long i = 0; i + (1 << k) <= n; i++) {
 				st[i][k] = f(st[i][k - 1], st[i + (1 << (k - 1))][k - 1]);
 			}
 		}
 	}
 	
-	/*Long query(Long l, Long r){ //O(|f| log n)
+	/*Long query(Long l, Long r) { //O(|f| log n)
 		Long ans = st[l][0];
-		if(l == r) return ans;
+		if (l == r) return ans;
 		l++;
 		Long range = r - l + 1;
 		
-		while( range > 0){
+		while ( range > 0) {
 			Long step = range & -range;
 			Long j = __builtin_ctz(step);
 			ans = f(ans , st[l][j]);
@@ -47,11 +47,11 @@ struct SparseTable{
 		return ans;
 	}*/
 	
-	Long query(Long L, Long R){ //O(|f|)
-		//special cases : idempotent(  min, max, gcd)
-		Long T = R - L + 1;
-		Long lg = 31 - (__builtin_clz(T));
-		return f(st[L][lg], st[R - (1LL << lg) + 1][lg]);
+	Long query(Long l, Long r) { //O(|f|)
+		//special cases : idempotent(min, max, gcd)
+		Long sz = r - l + 1;
+		Long lg = 31 - (__builtin_clz(sz));
+		return f(st[l][lg], st[r - (1 << lg) + 1][lg]);
 	}
 }st;
 
