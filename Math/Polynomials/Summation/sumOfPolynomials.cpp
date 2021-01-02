37e496bf97f0e065cdeb3e20231fdad790208bc7
Diego <dhurtadodemendoza@gmail.com>
2021-01-02 00:48:57 -0500

    refactoring of summation

--- Math/Polynomials/Summation/sumOfPolynomials.cpp
+++ Math/Polynomials/Summation/sumOfPolynomials.cpp
@@ -40,30 +40,6 @@ Long divide(Long a, Long b) {
 	return mult(a , modInverse(b));
 }
 
-const Long MX = 5002;
-
-Long stirling[MX][MX];
-
-void precalc() {
-	stirling[0][0] = 1;
-	for (Long n = 1; n < MX; n++) {
-		stirling[n][0] = 0;
-	}
-	for (Long n = 1; n < MX; n++) {
-		for (Long k = 1; k <= n; k++) {
-			stirling[n][k] = add(stirling[n - 1][k - 1] , mult(k , stirling[n - 1][k]));
-		}
-	}
-}
-
-polynomial transformFalling(Long c, Long n) { //O(n)
-	polynomial p(n + 1, 0);
-	for (Long i = 0; i <= n; i++) {
-		p[i] = mult(stirling[n][i], c);
-	}
-	return p;
-}
-
 polynomial operator +(const polynomial &A, const polynomial &B) {
 	polynomial ans(max(A.size(), B.size()));
 	for (Long i = 0; i < max(A.size(), B.size()); i++) {
@@ -80,42 +56,78 @@ polynomial operator +(const polynomial &A, const polynomial &B) {
 	return ans;
 }
 
-Long summation(polynomial &p, Long r) { //O(n^2)
-	//call precalc first
-	r %= MOD;
-	polynomial fallings;
-	for (Long i = 0; i < p.size(); i++) {
-		fallings = fallings + transformFalling(p[i], i);
-	}
-	Long ans = 0;
-	Long fact = 1;
-	Long x = r + 1;
-	for (Long i = 0; i < fallings.size(); i++) {
-		fact = mult(fact , x);
-		x--;
-		Long coef = divide(fallings[i], i + 1);
-		ans = add(ans, mult(coef, fact)) ;
-	}
-	return ans;
-}
+const Long MX = 5002;
 
+struct Summation {
+	Long stirling[MX][MX];
+	polynomial coef;
+	
+	void precalc() { //O(n^2)
+		stirling[0][0] = 1;
+		for (Long n = 1; n < MX; n++) {
+			stirling[n][0] = 0;
+		}
+		for (Long n = 1; n < MX; n++) {
+			for (Long k = 1; k <= n; k++) {
+				stirling[n][k] = add(stirling[n - 1][k - 1] , mult(k , stirling[n - 1][k]));
+			}
+		}
+	}
+	
+	Summation() {
+		precalc();
+	}
+
+	polynomial transformFalling(Long c, Long n) { //O(n)
+		polynomial p(n + 1, 0);
+		for (Long i = 0; i <= n; i++) {
+			p[i] = mult(stirling[n][i], c);
+		}
+		return p;
+	}
+	
+	void build(polynomial &p) { //O(n^2)
+		polynomial fallings;
+		for (Long i = 0; i < p.size(); i++) {
+			fallings = fallings + transformFalling(p[i], i);
+		}
+		coef = vector<Long>(fallings.size());
+		for (Long i = 0; i < fallings.size(); i++) {
+			coef[i] = divide(fallings[i], i + 1);
+		}
+	}
+
+	Long query(Long x) {  //O(n)
+		x %= MOD;
+		Long ans = 0;
+		Long fact = 1;
+		Long val = x + 1;
+		for (Long i = 0; i < coef.size(); i++) {
+			fact = mult(fact , val);
+			val--;
+			ans = add(ans, mult(coef[i], fact)) ;
+		}
+		return ans;
+	}
+}summation;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	precalc();
+	
 	Long n;
 	cin >> n;
 	polynomial p(n + 1);
 	for (Long i = n; i >= 0; i--) {
 		cin >> p[i];
 	}
+	summation.build(p);
 	while(true) {
 		Long x;
 		cin >> x;
 		if (x < 0) break;
-		cout << summation(p, x) << endl;
+		cout << summation.query(x) << endl;
 	}
 
 	return 0;
