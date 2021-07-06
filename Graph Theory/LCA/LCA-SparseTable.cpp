d30e0b42570d07e24df656d0b0ac3fc19a6f0ea8
Diego <dhurtadodemendoza@gmail.com>
2021-07-06 12:18:06 -0500

    updating sparse table in lca

--- Graph Theory/LCA/LCA-SparseTable.cpp
+++ Graph Theory/LCA/LCA-SparseTable.cpp
@@ -1,67 +1,62 @@
 #include <bits/stdc++.h>
 #define debug(x) cout << #x << " = " << x << endl
 #define REP(i,n) for(Long i = 0; i < (Long)n; i++)
-#define pb push_back
 using namespace std;
 
 typedef long long Long;
 
-const Long MX = 1e5;
-const Long LG = 32 - __builtin_clz(MX);
+const int MX = 1e5;
+const int LG = 32 - __builtin_clz(MX);
 
 struct SparseTable{
-	pair<Long,Long> st[2 * MX][LG + 1]; //<min depth , node>
+	pair<int, int> st[2 * MX][LG + 1]; //<min depth , node>
 	
-	pair<Long,Long> f(pair<Long,Long> a, pair<Long,Long> b){
-		return min(a , b);
-	}
-	
-	void build(vector<pair<Long,Long>> &A){ // O(n log n)
-		Long n = A.size();
-		for(Long i = 0; i < n; i++){
+	void build(vector<pair<int, int>> &A){ // O(n log n)
+		int n = A.size();
+		for(int i = 0; i < n; i++){
 			st[i][0] = A[i];
 		}
 		
-		for(Long j = 1; (1 << j) <= n; j++){
-			for(Long i = 0; i + (1 << j) <= n; i++){
-				st[i][j] = f(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
+		for(int j = 1; (1 << j) <= n; j++){
+			for(int i = 0; i + (1 << j) <= n; i++){
+				st[i][j] = min(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
 			}
 		}
 	}
 	
-	Long query(Long L, Long R){ //O(1)
-		Long T = R - L + 1;
-		Long lg = 31 - (__builtin_clz(T));
-		return f(st[L][lg], st[R- (1LL << lg) + 1][lg]).second;
+	int query(int l, int r){ //O(1)
+		int sz = r - l + 1;
+		int lg = 31 - (__builtin_clz(sz));
+		return min(st[l][lg], st[r - (1 << lg) + 1][lg]).second;
 	}
 	
 }st;
 
 struct Graph {
-	vector<Long> adj[MX];
-	vector<pair<Long,Long>> euler;
-	Long first[MX];
-	Long depth[MX];
-	Long tIn[MX];
-	Long tOut[MX];
-	Long timer;
+	vector<int> adj[MX];
+	vector<pair<int, int>> euler;
+	int first[MX];
+	int depth[MX];
+	int tIn[MX];
+	int tOut[MX];
+	int timer;
 	
-	void clear(Long n){
-		for(Long i = 0; i < n; i++){
+	void clear(int n){
+		for(int i = 0; i < n; i++){
 			adj[i].clear();
 		}
 	}
 	
-	void addEdge(Long u , Long v){
+	void addEdge(int u, int v){
 		adj[u].push_back(v);
 		adj[v].push_back(u);
 	}
 	
-	void dfs(Long u = 0, Long p = -1){ //O(n)
+	void dfs(int u = 0, int p = -1){ //O(n)
 		tIn[u] = timer++;
 		first[u] = euler.size();
 		euler.push_back({depth[u], u });
-		for ( Long v : adj[u] ) {
+		for (int v : adj[u] ) {
 			if(p != v){
 				depth[v] = depth[u] + 1;
 				dfs(v , u);
@@ -71,7 +66,8 @@ struct Graph {
 		tOut[u] = timer++;
 	}
 
-	void precalculate( Long root = 0){ //O(nlogn)
+	void precalculate(){ //O(nlogn)
+		int root = 0;
 		euler.clear();
 		depth[root] = 0;
 		timer = 0;
@@ -79,11 +75,11 @@ struct Graph {
 		st.build(euler);
 	}
 	
-	Long lca(Long u, Long v){ //O(1)
+	int lca(int u, int v){ //O(1)
 		return st.query(min(first[u] , first[v]) , max(first[u] , first[v]));
 	}
 	
-	bool isAncestor(Long u, Long v){ //is u ancestor of v ?
+	bool isAncestor(int u, int v){ //is u ancestor of v ?
 		return tIn[u] <= tIn[v] && tOut[u] >= tOut[v];
 	}	
 	
