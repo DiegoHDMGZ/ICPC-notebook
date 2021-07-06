882e96972829d3cdf1d8112b1fce9e55f4c7af07
Diego <dhurtadodemendoza@gmail.com>
2021-07-06 12:20:23 -0500

    deleting unnecessary space

--- Graph Theory/LCA/LCA-SparseTable.cpp
+++ Graph Theory/LCA/LCA-SparseTable.cpp
@@ -16,7 +16,6 @@ struct SparseTable{
 		for(int i = 0; i < n; i++){
 			st[i][0] = A[i];
 		}
-		
 		for(int j = 1; (1 << j) <= n; j++){
 			for(int i = 0; i + (1 << j) <= n; i++){
 				st[i][j] = min(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
@@ -29,7 +28,6 @@ struct SparseTable{
 		int lg = 31 - (__builtin_clz(sz));
 		return min(st[l][lg], st[r - (1 << lg) + 1][lg]).second;
 	}
-	
 }st;
 
 struct Graph {
