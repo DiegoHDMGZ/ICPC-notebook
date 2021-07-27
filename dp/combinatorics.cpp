ae6afd85faf73a29dc7d2eeb0faea4b7b8ab0e0f
Diego <dhurtadodemendoza@gmail.com>
2021-07-26 20:01:53 -0500

    modulo in dp combinatorics

--- dp/combinatorics.cpp
+++ dp/combinatorics.cpp
@@ -1,30 +1,26 @@
 #include <bits/stdc++.h>
-#define debug(x) cout << #x << " = " << x << endl
-#define REP(i,n) for(Long i = 0; i < (Long)n; i++)
-#define pb push_back
 using namespace std;
 
 typedef long long Long;
 
 const Long MX = 1000;
+const Long MOD = 1e9 + 7;
+Long add(Long a, Long b) {
+	if (a + b < MOD) return a + b;
+	return a + b - MOD;
+}
+
 Long dp[MX][MX];
 bool used[MX][MX];
 Long comb(Long n , Long m) {
-	if(n == m){ 
-		return 1;
-	}
-	if(m == 0) {
-		return 1;
-	}
-	
-	if(used[n][m]) {
-		return dp[n][m];
-	}
+	if (n < m) return 0;
+	if(n == m) return 1;
+	if(m == 0) return 1;
+	if(used[n][m]) return dp[n][m];
 	used[n][m] = true;
-	return dp[n][m] = comb(n - 1 , m - 1) + comb(n - 1 , m );
+	return dp[n][m] = add(comb(n - 1, m - 1), comb(n - 1, m));
 }
 
 int main() {
 	return 0;
 }
-
