a0512392a38826256458aec31c102c379282ca0d
Diego <dhurtadodemendoza@gmail.com>
2021-08-01 19:02:41 -0500

    improved readability in pick's theorem

--- Math/Geometry/Basics/picksTheorem.cpp
+++ Math/Geometry/Basics/picksTheorem.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 #define debug(x) cout << #x << " = " << x << endl
 #define REP(i,n) for(Long i = 0; i < (Long)n; i++)
-#define pb push_back
 using namespace std;
 
 typedef long long Long;
@@ -10,68 +9,27 @@ typedef long long Long;
 // I = (2A - B + 2) / 2
 
 struct Point{
-	Long x,y;
-	
-	Point(){
-		x = 0;
-		y = 0;
-	}
-	Point(Long _x, Long _y){
-		x = _x, y = _y;
-	}
-
-	Point operator += (const Point &t){
-		x += t.x;
-		y += t.y;
-		return *this;
-	}
-	Point operator -= (const Point &t){
-		x -= t.x;
-		y -= t.y;
-		return *this;
-	}
-	Point operator *= (Long t){
-		x *= t;
-		y *= t;
-		return *this;
-	}
-	Point operator +(const Point &t) const {
-		return Point(*this) += t;
-	}
-	Point operator - (const Point &t) const{
-		return Point(*this) -= t;
-	}
-
-	Long cross(const Point &P) const {
-		return x * P.y - y * P.x;
-	}
-	
-	Long cross(const Point &a , const Point &b) const {
-		return (a - *this).cross(b - *this);
-	}
-	
-	void debugPoint(string nombre){
-		cout << nombre << " = ( " << fixed << setprecision(2) <<  x << " , " << y  << " ) " << endl; 
-	}
+	Long x, y;
+	Point(Long x, Long y) : x(x), y(y){}
 };
 
 Long area2(vector<Point> &v) { //O(n)
-	//el doble del area
+	//double of the real area
 	Long ans = 0;
 	Long sz = v.size();
 	for(Long i = 0; i < sz; i++) {
-		Point P = v[(i - 1 + sz) % sz ];
+		Point P = v[(i - 1 + sz) % sz];
 		Point Q = v[i];
 		ans += (P.x - Q.x) * (P.y + Q.y);
 	}
 	return abs(ans);
 }
 
-Long boundaryPoints(Point A, Point B) { //O(1)
+Long boundaryPoints(Point A, Point B) { //O(log MX)
 	return __gcd(abs(A.x - B.x) , abs(A.y - B.y)) + 1;
 }
 
-Long boundaryPoints(vector<Point> &v) { //O(n)
+Long boundaryPoints(vector<Point> &v) { //O(n log MX)
 	Long n = v.size();
 	Long ans = 0;
 	for(Long i = 0; i < n; i++) {
@@ -80,30 +38,10 @@ Long boundaryPoints(vector<Point> &v) { //O(n)
 	return ans - n;
 }
 
-Long interiorPoints(vector<Point> &v) { //O(n)
+Long interiorPoints(vector<Point> &v) { //O(n log MX)
 	return (area2(v) - boundaryPoints(v) + 2) / 2;
 }
 
-
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	Long n;
-	cin >> n;
-	vector<Point> v;
-	REP(i , n) {
-		Point P;
-		cin >> P.x >> P.y;
-		v.pb(P);
-	}
-	double A = area2(v) / 2.0;
-	debug(A);
-	Long B = boundaryPoints(v);
-	debug(B);
-	Long I = interiorPoints(v);
-	debug(I);
-	
 	return 0;
 }
-
