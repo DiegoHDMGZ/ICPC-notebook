1d1674df8f3dc82140348148705d6dc653cb44d3
Diego <dhurtadodemendoza@gmail.com>
2021-10-11 23:14:32 -0500

    minor changes in count multiples

--- Math/inclusionExclusion/multiples.cpp
+++ Math/inclusionExclusion/multiples.cpp
@@ -1,50 +1,37 @@
 #include <bits/stdc++.h>
 #define debug(x) cout << #x << " = " << x << endl
 #define REP(i,n) for(Long i = 0; i < (Long)n; i++)
-#define pb push_back
 using namespace std;
 
 typedef long long Long;
 
-Long LCM(Long a, Long b){
-	return a/__gcd(a,b) * b;
+int getBit(int mask, int i){
+	return (mask >> i) & 1;
 }
 
-Long getBit(Long x, Long i){
-	return (x >> i) & 1LL;
-}
-
-Long multiplos(Long N , vector<Long> &v) { 
-//Count the numbers of integers in [1 , N] that are multiples of at least
-//one of the v[i]
-	Long sz = v.size();
-	Long resp = 0;
-	for(Long mask = 1; mask < (1LL << sz); mask++ ) {
+Long countMultiples(Long N, vector<Long> &v) { 
+	//Count the numbers of integers in [1 , n] 
+	//that are multiples of at least one of the v[i]
+	int sz = v.size();
+	Long ans = 0;
+	for (int mask = 1; mask < (1 << sz); mask++) {
 		Long cnt = 0;
 		Long den = 1;
-		for(Long i = 0; i < sz; i++){
-			
-			if(getBit(mask, i) == 1) {
-				cnt ++;
-				den = LCM(den , v[i]);
+		for (int i = 0; i < sz; i++){
+			if (getBit(mask, i) == 1) {
+				cnt++;
+				den = lcm(den , v[i]);
 			}
 		}
-		
-		if(cnt % 2 == 0){
-			resp -= N / den;
-		}
-		else{
-			resp += N / den;
+		if (cnt % 2 == 0) {
+			ans -= N / den;
+		} else{
+			ans += N / den;
 		}
 	}
-	
-	return resp;
+	return ans;
 }
 
 int main() {
-
 	return 0;
 }
-
-
-
