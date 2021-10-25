84e61964f55d4078dd90ae3e4466f95320aeff0d
Diego <dhurtadodemendoza@gmail.com>
2021-10-25 16:51:28 -0500

    fix negative cycle search in bellman ford

--- Graph Theory/ShortestPath/BellmanFord/bellmanFord.cpp
+++ Graph Theory/ShortestPath/BellmanFord/bellmanFord.cpp
@@ -8,44 +8,46 @@ const Long MX = 1e4;
 const Long INF = 1e18;
 
 struct Graph{
-	vector<pair<Long,Long>> adj[MX];
+	vector<pair<Long, Long>> adj[MX];
 	Long d[MX];
-	Long parent[MX];
+	int parent[MX];
+	int reachedBy[MX];
 	
-	void clear(Long n) {
+	void clear(int n) {
 		for (int i = 0; i < n; i++) {
 			adj[i].clear();
 		}
 	}
 
-	void addEdge(Long u, Long v, Long w) {
+	void addEdge(int u, int v, Long w) {
 		adj[u].push_back({v, w});
 	}
 	
-	void dfs(Long u) {
+	void dfs(int u, int root) {
 		d[u] = -INF;
+		reachedBy[u] = root;
 		for (auto e : adj[u]) {
-			Long v = e.first;
+			int v = e.first;
 			if (d[v] != -INF) {
-				dfs(v);
+				dfs(v, root);
 			}
 		}
 	}
 	
-	bool bellmanFord(Long s , Long n){ //O(VE)
+	bool bellmanFord(int s , int n){ //O(VE)
 		//true : negative cycle found
-		for(Long i = 0; i < n; i++){
+		for(int i = 0; i < n; i++){
 			d[i] = INF;
 			parent[i] = -1;
 		}
 		d[s] = 0;
 		vector<bool> cycle(n, false);
 		bool tense;
-		for (Long i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			tense = false;
-			for (Long u = 0; u < n; u++) {
+			for (int u = 0; u < n; u++) {
 				for (auto e : adj[u]) {
-					Long v = e.first;
+					int v = e.first;
 					Long w = e.second;
 					if (d[u] != INF && d[u] + w < d[v]) { //tense
 						d[v] = d[u] + w; //relax
@@ -63,19 +65,20 @@ struct Graph{
 		}
 		for (int u = 0; u < n; u++) {
 			if (cycle[u] && d[u] != -INF) {
-				dfs(u);
+				dfs(u, u);
 			}
 		}
 		return tense;
 	}
 	
-	vector<Long> getNegativeCycle(Long u, Long n){
-		//go back n times to find a cycle
+	vector<int> getNegativeCycle(int u, int n){
 		assert(d[u] == -INF);
+		u = reachedBy[u];
+		//go back n times to find a cycle
 		for (int i = 0; i < n; i++) {
 			u = parent[u]; 
 		}
-		vector<Long> cycle = {u};
+		vector<int> cycle = {u};
 		u = parent[u];
 		while (u != cycle[0]) {
 			cycle.push_back(u);
@@ -86,11 +89,11 @@ struct Graph{
 		return cycle;
 	}
 	
-	vector<Long> getPath(Long u){
+	vector<int> getPath(int u){
 		if (d[u] == INF) {
 			return {};
 		}
-		vector<Long> path;
+		vector<int> path;
 		while(u != -1){
 			path.push_back(u);
 			u = parent[u];
