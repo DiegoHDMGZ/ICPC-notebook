4774c42253f06bd718c5859832cfe9e6ea870ba7
Diego <dhurtadodemendoza@gmail.com>
2021-10-25 16:51:58 -0500

    fix negative cycle search in spfa and change ending condition

--- Graph Theory/ShortestPath/BellmanFord/spfa.cpp
+++ Graph Theory/ShortestPath/BellmanFord/spfa.cpp
@@ -11,86 +11,81 @@ const Long INF = 1e18;
 struct Graph{
 	vector<pair<Long ,Long>> adj[MX];
 	Long d[MX];
-	Long parent[MX];
+	int parent[MX];
+	int reachedBy[MX];
 	
-	void clear(Long n) {
+	void clear(int n) {
 		for (int i = 0; i < n; i++) {
 			adj[i].clear();
 		}
 	}
 	
-	void addEdge(Long u, Long v, Long w) {
+	void addEdge(int u, int v, Long w) {
 		adj[u].push_back({v, w});
 	}
 	
-	void dfs(Long u) {
+	void dfs(int u, int root) {
 		d[u] = -INF;
+		reachedBy[u] = root;
 		for (auto e : adj[u]) {
-			Long v = e.first;
+			int v = e.first;
 			if (d[v] != -INF) {
-				dfs(v);
+				dfs(v, root);
 			}
 		}
 	}
 	
-	bool spfa(Long s, Long n){ //O(VE)
-		for(Long i = 0; i < n; i++){
+	bool spfa(int s, int n){ //O(VE)
+		for(int i = 0; i < n; i++){
 			d[i] = INF;
 			parent[i] = -1;
 		}
-		queue<Long> q;
+		queue<int> q;
 		vector<bool> inQueue(n , false);
-		vector<Long> cnt(n, 0);
+		vector<bool> cycle(n, false);
 		d[s] = 0;
 		inQueue[s] = true;
 		q.push(s);
-		bool negativeCycle = false;
-		while(!q.empty()){
-			Long u = q.front();
-			q.pop();
-			inQueue[u] = false;
-			cnt[u]++;
-			if(cnt[u] >= n){
-				negativeCycle = true;
-				break;
-			}
-			for(auto e : adj[u]){
-				Long v = e.first;
-				Long w = e.second;
-				if(d[u] + w < d[v]){
-					d[v] = d[u] + w;
-					parent[v] = u;
-					if(!inQueue[v]){
-						q.push(v);
-						inQueue[v] = true;
+		int phase = 0;
+		while(!q.empty() && phase < n){
+			int sz = q.size();
+			REP(i, sz) {
+				int u = q.front();
+				q.pop();
+				inQueue[u] = false;
+				for(auto e : adj[u]){
+					int v = e.first;
+					Long w = e.second;
+					if(d[u] + w < d[v]){
+						d[v] = d[u] + w;
+						parent[v] = u;
+						if(!inQueue[v]){
+							q.push(v);
+							inQueue[v] = true;
+						}
+						if (phase == n - 1) {
+							cycle[v] = true;
+						}
 					}
 				}
 			}
+			phase++;
 		}
-		if (!negativeCycle) {
+		if (phase < n) {
 			return false;
-		}
-		vector<bool> cycle(n , false);
-		for (int u = 0; u < n; u++) {
-			for (auto e : adj[u]) {
-				Long v = e.first;
-				Long w = e.second;
-				if (d[u] != INF && d[u] + w < d[v]) {
-					cycle[v] = true;
-				}
-			}
 		}
 		for (int u = 0; u < n; u++) {
 			if (cycle[u] && d[u] != -INF) {
-				dfs(u);
+				dfs(u, u);
 			}
 		}
 		return true;
 	}
 	
 	vector<Long> getNegativeCycle(Long u, Long n){
-		//go back n times to find a cycle
 		assert(d[u] == -INF);
+		u = reachedBy[u];
+		//go back n times to find a cycle
 		for (int i = 0; i < n; i++) {
 			u = parent[u]; 
 		}
