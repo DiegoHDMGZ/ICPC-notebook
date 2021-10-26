c16c14a8e3690bbcb769fbe0af6b63786d8dbcbd
Diego <dhurtadodemendoza@gmail.com>
2021-10-25 23:42:56 -0500

    fix style

--- Graph Theory/StronglyConnectedComponents/kosaraju.cpp
+++ Graph Theory/StronglyConnectedComponents/kosaraju.cpp
@@ -1,28 +1,27 @@
 #include <bits/stdc++.h>
 #define debug(x) cout << #x << " = " << x << endl
 #define REP(i,n) for(Long i = 0; i < (Long)n; i++)
-#define pb push_back
 
 using namespace std;
 
 typedef long long Long;
 const Long MX = 1e5;
 
-// In a directed graph , a Strongly connected component (SCC) is a maximal subgraph 
+//In a directed graph , a Strongly connected component (SCC) is a maximal subgraph 
 //such that each vertex is reachable from every other vertex
 //The graph can be condensed in their scc to form a DAG
 
 struct Graph {
-	vector <Long> adj[MX];
-	vector <Long> rev[MX];
+	vector<Long> adj[MX];
+	vector<Long> rev[MX];
 	bool vis1[MX];
 	bool vis2[MX];
 	vector<Long> order,component;
 	Long numComponent = 0;
 	Long scc[MX];
 	
-	void clear(Long N = MX) {
-		REP( i , N) {
+	void clear(Long n) {
+		REP(i , n) {
 			adj[i].clear();
 			rev[i].clear();
 			vis1[i] = vis2[i] = false;
@@ -33,49 +32,46 @@ struct Graph {
 	}
 	
 	void addEdge(Long u, Long v) {
-		adj[u].pb(v);
-		rev[v].pb(u);
+		adj[u].push_back(v);
+		rev[v].push_back(u);
 	}
 	
-	void dfs1(Long u){ //O(N + M)
+	void dfs1(Long u){ //O(V + E)
 		vis1[u] = true;
-		for(Long v : adj[u]) {
-			if(!vis1[v]) {
+		for (Long v : adj[u]) {
+			if (!vis1[v]) {
 				dfs1(v);
 			}
 		}
-		order.pb(u);
+		order.push_back(u);
 	}
 	
-	void dfs2(Long u) { //O(N + M)
+	void dfs2(Long u) { //O(V + E)
 		vis2[u] = true;
-		component.pb(u);
-		for(Long v : rev[u]) {
-			if(!vis2[v]) {
+		component.push_back(u);
+		for (Long v : rev[u]) {
+			if (!vis2[v]) {
 				dfs2(v);
 			}
 		}
 	}
 	
-	void calculateSCC(Long N) { //O(N + M)
-		REP(i, N){
-			if(!vis1[i]) dfs1(i);
+	void calculateSCC(Long n) { //O(V + E)
+		REP(i, n) {
+			if (!vis1[i]) dfs1(i);
 		}
-		REP(i , N ) {
-			Long u = order[N - 1 - i];
-			if(!vis2[u]) {
+		REP(i, n) {
+			Long u = order[n - 1 - i];
+			if (!vis2[u]) {
 				dfs2(u);
-				
-				for(Long v : component) {
+				for (Long v : component) {
 					scc[v] = numComponent;
 				}
 				numComponent++;
 				//all nodes in "component" form a scc
 				component.clear();
 			}
-			
 		}
-		
 	}
 } G;
 
