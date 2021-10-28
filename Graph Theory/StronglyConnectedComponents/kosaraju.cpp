00db575da5397580ca82d35a643a08483f9fbd26
Diego <dhurtadodemendoza@gmail.com>
2021-10-27 22:34:51 -0500

    simplify kosaraju code

--- Graph Theory/StronglyConnectedComponents/kosaraju.cpp
+++ Graph Theory/StronglyConnectedComponents/kosaraju.cpp
@@ -12,64 +12,53 @@ const Long MX = 1e5;
 //The graph can be condensed in their scc to form a DAG
 
 struct Graph {
-	vector<Long> adj[MX];
-	vector<Long> rev[MX];
-	bool vis1[MX];
-	bool vis2[MX];
-	vector<Long> order,component;
-	Long numComponent = 0;
-	Long scc[MX];
+	vector<int> adj[MX];
+	vector<int> rev[MX];
+	bool vis[MX];
+	vector<int> order;
+	int numComponent;
+	int scc[MX];
 	
 	void clear(Long n) {
-		REP(i , n) {
+		REP(i, n) {
 			adj[i].clear();
 			rev[i].clear();
-			vis1[i] = vis2[i] = false;
+			vis[i] = false;
 		}
-		order.clear();
-		component.clear();
-		numComponent = 0;
 	}
 	
-	void addEdge(Long u, Long v) {
+	void addEdge(int u, int v) {
 		adj[u].push_back(v);
 		rev[v].push_back(u);
 	}
 	
-	void dfs1(Long u){ //O(V + E)
-		vis1[u] = true;
-		for (Long v : adj[u]) {
-			if (!vis1[v]) {
-				dfs1(v);
-			}
+	void dfs(int u) { //O(V + E)
+		vis[u] = true;
+		for (int v : adj[u]) {
+			if (!vis[v]) dfs(v);
 		}
 		order.push_back(u);
 	}
 	
-	void dfs2(Long u) { //O(V + E)
-		vis2[u] = true;
-		component.push_back(u);
-		for (Long v : rev[u]) {
-			if (!vis2[v]) {
-				dfs2(v);
-			}
+	void findComponent(int u) { //O(V + E)
+		scc[u] = numComponent;
+		for (int v : rev[u]) {
+			if (scc[v] == -1) findComponent(v);
 		}
 	}
 	
-	void calculateSCC(Long n) { //O(V + E)
-		REP(i, n) {
-			if (!vis1[i]) dfs1(i);
+	void calculateSCC(int n) { //O(V + E)
+		order.clear();
+		numComponent = 0;
+		for (int u = 0; u < n; u++) {
+			if (!vis[u]) dfs(u);
+			scc[u] = -1;
 		}
-		REP(i, n) {
-			Long u = order[n - 1 - i];
-			if (!vis2[u]) {
-				dfs2(u);
-				for (Long v : component) {
-					scc[v] = numComponent;
-				}
+		for (int i = 0; i < n; i++) {
+			int u = order[n - 1 - i];
+			if (scc[u] == -1) {
+				findComponent(u);
 				numComponent++;
-				//all nodes in "component" form a scc
-				component.clear();
 			}
 		}
 	}
