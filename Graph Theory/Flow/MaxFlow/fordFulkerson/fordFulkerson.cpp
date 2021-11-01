2b8b5410e6fbb36df7d732600fb5f2490326b1db
Diego <dhurtadodemendoza@gmail.com>
2021-11-01 18:13:43 -0500

    fix spaces in max flow

--- Graph Theory/Flow/MaxFlow/fordFulkerson/fordFulkerson.cpp
+++ Graph Theory/Flow/MaxFlow/fordFulkerson/fordFulkerson.cpp
@@ -16,11 +16,11 @@ struct Graph{
 	bool vis[MX];
 	bool added[MX][MX];
 	
-	void clear(Long N = MX){
-		for(Long i = 0 ; i < N; i++){
+	void clear(Long n) {
+		for (Long i = 0 ; i < n; i++) {
 			adj[i].clear();
 			vis[i] = false;
-			for(Long j = 0; j < N; j++) {
+			for (Long j = 0; j < n; j++) {
 				cap[i][j] = 0;
 				flow[i][j] = 0;
 				added[i][j] = false;
@@ -28,31 +28,27 @@ struct Graph{
 		}
 	}
 	
-	void addEdge(Long u, Long v, Long w, bool dir ){
-		if(!added[min(u, v)][max(u , v)]) {
+	void addEdge(Long u, Long v, Long w, bool dir) {
+		if (!added[min(u, v)][max(u , v)]) {
 			adj[u].push_back(v);
 			adj[v].push_back(u);
 		}
 		added[min(u , v)][max(u , v)] = true;
 		cap[u][v] += w;
-		if(!dir){
+		if (!dir) {
 			cap[v][u] += w;
 		}
-		
 	}
 	
-	Long dfs(Long u, Long t ,Long f){ //O(E)
-		if(u == t) return f;
-		if(vis[u]) return 0;
+	Long dfs(Long u, Long t ,Long f) { //O(E)
+		if (u == t) return f;
+		if (vis[u]) return 0;
 		vis[u] = true;
-		
-		for( Long v : adj[u] ) {
+		for (Long v : adj[u]) {
 			Long cf = cap[u][v] - flow[u][v];
-			if(cf == 0) continue;
-			
-			Long ret = dfs(v, t, min(f, cf) );
-			
-			if(ret > 0){
+			if (cf == 0) continue;
+			Long ret = dfs(v, t, min(f, cf));
+			if (ret > 0) {
 				flow[u][v] += ret;
 				flow[v][u] -= ret;
 				return ret;
@@ -61,9 +57,9 @@ struct Graph{
 		return 0;
 	}
 	
-	Long maxFlow(Long s, Long t, Long n){ //O(E |F|) = O(E * nU)
+	Long maxFlow(Long s, Long t, Long n) { //O(E |F|) = O(E * nU)
 		Long ans = 0;
-		while(true){ // O(|F|) iterations
+		while (true) { // O(|F|) iterations
 			fill(vis, vis + n, false);
 			Long inc = dfs(s, t, INF);
 			if(inc == 0) break;
@@ -71,26 +67,8 @@ struct Graph{
 		}
 		return ans;
 	}
-	
 } G;
 
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	Long n,m;
-	cin >> n >> m;
-	
-	REP( i , m){
-		Long u,v,c;
-		cin >> u >> v >> c;
-		u--; v--;
-		G.addEdge(u,v,c , false);//undirected
-		//addEdge(u,v,c,true); //directed
-	}
-	
-	Long s,t;
-	cin >> s >> t;
-	Long resp = G.maxFlow(s, t, n);
-	
 	return 0;
 }
