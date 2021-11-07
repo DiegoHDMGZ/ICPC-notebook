67b59a09b2185e455c7b1fb08b8bb1d615c0f465
Diego <dhurtadodemendoza@gmail.com>
2021-11-07 13:37:06 -0500

    refactor heavy light decomposition

--- Graph Theory/Heavy Light/heavyLight.cpp
+++ Graph Theory/Heavy Light/heavyLight.cpp
@@ -1,35 +1,44 @@
 #include <bits/stdc++.h>
 #define debug(x) cout << #x << " = " << x << endl
 #define REP(i,n) for(Long i = 0; i < (Long)n; i++)
-#define pb push_back
 using namespace std;
 
 typedef long long Long;
 
 //https://cp-algorithms.com/graph/hld.html
 
+//Querys and updates on tree paths
 //With weight on edges, put the weight in the node with greater depth and add one line to query function
 
-const Long MX = 1e5;
-Long segmentTreeQuery(Long pos1 , Long pos2) { 
-	return 0;
+const int MX = 2e5;
+
+Long combine(Long a, Long b) {
+	return max(a, b);
 }
 
-void segmentTreeUpdate(Long pos, Long v){
-	return;
-}
+struct SegmentTree {
+	Long query(int l, int r) {
+		return 0;
+	}
+	
+	void update(int pos, Long val) {
+		//range updates are also possible
+		return;
+	}
+} st;
+
 
 struct Graph {
-	vector <Long> adj[MX];
-	Long parent[MX];
-	Long depth[MX];
-	Long heavy[MX];
-	Long head[MX];
-	Long pos[MX];
-	Long curPos = 0;
+	vector<int> adj[MX];
+	int parent[MX];
+	int depth[MX];
+	int heavy[MX];
+	int head[MX];
+	int pos[MX];
+	int curPos = 0;
 	
-	void clear(Long N = MX) { //O(N)
-		REP( i , N) {
+	void clear(int n) { //O(n)
+		REP(i , n) {
 			adj[i].clear();
 			parent[i] = -1;
 			heavy[i] = -1;
@@ -38,82 +47,76 @@ struct Graph {
 		curPos = 0;
 	}
 	
-	void addEdge(Long u, Long v) { //O(1)
-		adj[u].pb(v);
-		adj[v].pb(u);
+	void addEdge(int u, int v) { //O(1)
+		adj[u].push_back(v);
+		adj[v].push_back(u);
 	}
 	
-	Long dfs(Long u = 0){ //O(N + M)
-		Long sz = 1;
-		Long maxSize = 0;
+	int dfs(int u = 0) { //O(V + E)
+		int sz = 1;
+		int maxSize = 0;
 		heavy[u] = -1;
-		for(Long v : adj[u]){
-			if(v != parent[u]) {
+		for (int v : adj[u]) {
+			if (v != parent[u]) {
 				parent[v] = u;
 				depth[v] = depth[u] + 1;
-				Long szV = dfs(v);
+				int szV = dfs(v);
 				sz += szV;
-				if(szV > maxSize) {
+				if (szV > maxSize) {
 					maxSize = szV;
 					heavy[u] = v;
 				}
 			}
 		}
-		
 		return sz;
 	}
 	
-	void decompose(Long u , Long h ) { //O(N + M)
+	void decompose(int u, int h) { //O(V + E)
 		head[u] = h;
 		pos[u] = curPos++;
-		if(heavy[u] != -1) {
-			decompose(heavy[u] , h);
+		if (heavy[u] != -1) {
+			decompose(heavy[u], h);
 		}
-		
-		for(Long v : adj[u]) {
-			if(v != parent[u] && v != heavy[u] ) {
-				decompose(v , v);
+		for (int v : adj[u]) {
+			if (v != parent[u] && v != heavy[u]) {
+				decompose(v, v);
 			}
 		}
 	}
 	
-	void build(vector<Long> &v , Long root = 0){
+	void build(vector<Long> &v, int root = 0) {
 		curPos = 0;
 		depth[root] = 0;
 		parent[root] = -1;
 		dfs(root);
-		decompose(root , root);
-		for(Long i = 0; i < v.size(); i++){
-			segmentTreeUpdate(pos[i] , v[i]);
+		decompose(root, root);
+		for (int i = 0; i < v.size(); i++) {
+			st.update(pos[i] , v[i]);
 		}
 	}
 	
-	Long query(Long u , Long v) {	//O( O( data query) log N)
+	Long query(int u, int v) {	//O(O(|query|) log V)
 		Long ans = 0;
-		
-		while(head[u] != head[v]) {
-			if(depth[head[u]] > depth[head[v]]) {
+		while (head[u] != head[v]) {
+			if (depth[head[u]] > depth[head[v]]) {
 				swap(u , v);
 			}
-			
-			Long curHeavyMax = segmentTreeQuery(pos[head[v]], pos[v]);
-			ans = max(ans , curHeavyMax);
-			
+			Long curHeavyVal = st.query(pos[head[v]], pos[v]);
+			ans = combine(ans, curHeavyVal);
 			v = parent[head[v]];
 		}
-		
-		if(depth[u] > depth[v]) {
-			swap(u , v);
-		}
-		
+		if (depth[u] > depth[v]) swap(u , v);
 		//u = heavy[u]; //when weight are on edges
 		
 		//last heavy path
-		Long lastMax = segmentTreeQuery(pos[u], pos[v]);
-		ans = max(ans, lastMax);
+		Long lastVal = st.query(pos[u], pos[v]);
+		ans = combine(ans, lastVal);
 		return ans;
 	}
-
+	
+	void update(int u, Long val) {
+		st.update(pos[u], val);
+	}
 } G;
 
 int main() {
