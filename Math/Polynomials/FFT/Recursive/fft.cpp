3d3223b8a0c3b091e43682df8ed552ab78e6804b
Diego <dhurtadodemendoza@gmail.com>
2021-12-23 18:29:36 -0500

    refactor fft and ntt

--- Math/Polynomials/FFT/Recursive/fft.cpp
+++ Math/Polynomials/FFT/Recursive/fft.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 #define debug(x) cout << #x << " = " << x << endl
 #define REP(i,n) for(Long i = 0; i < (Long)n; i++)
-#define pb push_back
 using namespace std;
 
 typedef long long Long;
@@ -10,30 +9,21 @@ typedef complex<Double> Complex;
 
 const Double PI = acos(-1);
 
-void fft(vector<Complex> &a, bool invert){ //O(n log n)
+void fft(vector<Complex> &a, Complex wn) { //O(n log n)
 	//n must be a power of 2
-	Long n = a.size();
-	if(n == 1){
-		return;
-	}
-	Long sgn = 1;
-	if(invert) sgn = -1;
-	Complex wn = polar((Double)1 , sgn * 2.0 * PI / n);
-	Complex w(1);
+	int n = a.size();
+	if (n == 1) return;
 	vector<Complex> even(n / 2), odd(n / 2);
-	for(Long i = 0; 2 * i < n; i++){
+	for (int i = 0; 2 * i < n; i++) {
 		even[i] = a[2 * i];
 		odd[i] = a[2 * i + 1];
 	}
-	fft(even, invert);
-	fft(odd, invert);
-	for(Long i = 0; 2 * i < n; i++){
+	fft(even, wn * wn);
+	fft(odd, wn * wn);
+	Complex w(1);
+	for (int i = 0; 2 * i < n; i++) {
 		a[i] = even[i] + w * odd[i];
 		a[i + n / 2] = even[i] - w * odd[i];
-		if(invert){
-			a[i] /= 2;
-			a[i + n / 2] /= 2;
-		}
 		w *= wn;
 	}
 }
@@ -44,31 +34,26 @@ polynomial operator *(const polynomial &a, const polynomial &b) {
 	vector<Complex> fa(a.begin(), a.end());
 	vector<Complex> fb(b.begin(), b.end());
 	Long n = 1;
-	while(n < a.size() + b.size()){
-		n <<= 1;
-	}
+	while (n < a.size() + b.size()) n <<= 1;
 	fa.resize(n);
 	fb.resize(n);
 	
-	fft(fa, false);
-	fft(fb, false);
+	Complex wn = polar((Double)1 , 2.0 * PI / n);
+	fft(fa, wn);
+	fft(fb, wn);
 	
-	for(Long i = 0; i < n; i++){
+	for (int i = 0; i < n; i++) {
 		fa[i] *= fb[i];
+		fa[i] /= n; //inverse
 	}
-	fft(fa, true);
+	wn = polar((Double)1 , -2.0 * PI / n); //inverse
+	fft(fa, wn);
 	
 	polynomial ans(a.size() + b.size() - 1);
-	for(Long i = 0; i < ans.size(); i++){
-		ans[i] = round(fa[i].real());
-	}
+	for (int i = 0; i < ans.size(); i++) ans[i] = round(fa[i].real());
 	return ans;
 } 
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	
 	return 0;
 }
--- Math/Polynomials/FFT/Recursive/ntt.cpp
+++ Math/Polynomials/FFT/Recursive/ntt.cpp
@@ -1,86 +1,89 @@
 #include <bits/stdc++.h>
 #define debug(x) cout << #x << " = " << x << endl
 #define REP(i,n) for(Long i = 0; i < (Long)n; i++)
-#define pb push_back
 using namespace std;
 
 typedef long long Long;
 
 //MOD = 2^k * c + 1
-//r = primiteve root of MOD
+//r = primitive root of MOD
 //wn = r^c
 
 const Long MOD = 998244353; //MOD = 2^23 * 119 + 1
 
 const Long root = 3;//primitive root of MOD
-const Long rootInv = 332748118;//inverse modular of root
+const Long rootInv = 332748118;//modular inverse of root
 
-struct Field{
-	Long num;
-	Field(Long num = 0) : num(num){}
-	
-	Field operator + (const Field &F) const {
-		return (num + F.num) % MOD;
-	}
-	
-	Field operator - (const Field &F) const {
-		return (num - F.num + MOD) % MOD;
-	}
-	
-	Field operator * (const Field &F) const {
-		return (num * F.num) % MOD;
-	}
-	
-	Field operator *= (const Field &F)  {
-		num *= F.num;
-		num %= MOD;
-		return *this;
-	}
-};
-
-const Field inv2 = Field(499122177);//mod inverse of 2
-
-Long mult(Long a, Long b){
-	return (a * b ) % MOD;
+Long mult(Long a, Long b) {
+	return (a * b) % MOD;
 }
 
-Long fastPow(Long a, Long b ){ //O(logb)
+Long fastPow(Long a, Long b) { //O(log b)
 	Long ans = 1;
-	while(b > 0){
-		if(b & 1 == 1){ //b % 2 == 1
-			ans = mult(ans ,a );
-		}
-		a = mult(a , a  );
-		b >>= 1; //b /= 2;
+	while (b > 0) {
+		if (b & 1) ans = mult(ans, a);
+		a = mult(a, a);
+		b >>= 1;
 	}
 	return ans;
 }
 
+Long modInverse(Long a) {
+	return fastPow(a, MOD - 2);
+}
 
-void ntt(vector<Field> &a, bool invert, Field wn){ //O(n log n)
-	//n must be a power of 2
-	Long n = a.size();
-	if(n == 1){
-		return;
+Long divide(Long a, Long b) {
+	return mult(a, modInverse(b));
+}
+
+struct Field{
+	Long val;
+	Field(Long val = 0) {
+		this->val = val % MOD;
 	}
-
-	Field w (1);
-	vector<Field> even(n / 2), odd(n / 2);
 	
-	for(Long i = 0; 2 * i < n; i++){
+	Field operator +(const Field &F) const {
+		return (val + F.val) % MOD;
+	}
+	
+	Field operator -(const Field &F) const {
+		return (val - F.val + MOD) % MOD;
+	}
+	
+	Field operator *(const Field &F) const {
+		return mult(val, F.val);
+	}
+	
+	Field operator *=(const Field &F)  {
+		val = mult(val, F.val);
+		return *this;
+	}
+	
+	Field operator /(const Field &F) const {
+		return divide(val, F.val);
+	}
+	
+	Field operator /=(const Field &F)  {
+		val = divide(val, F.val);
+		return *this;
+	}
+};
+
+void ntt(vector<Field> &a, Field wn) { //O(n log n)
+	//n must be a power of 2
+	int n = a.size();
+	if (n == 1) return;
+	vector<Field> even(n / 2), odd(n / 2);
+	for (int i = 0; 2 * i < n; i++) {
 		even[i] = a[2 * i];
 		odd[i] = a[2 * i + 1];
 	}
-
-	ntt(even, invert, wn * wn);
-	ntt(odd, invert, wn * wn);
-	for(Long i = 0; 2 * i < n; i++){
+	ntt(even, wn * wn);
+	ntt(odd, wn * wn);
+	Field w(1);
+	for (int i = 0; 2 * i < n; i++) {
 		a[i] = even[i] + w * odd[i];
 		a[i + n / 2] = even[i] - w * odd[i];
-		if(invert){
-			a[i] *= inv2;
-			a[i + n / 2] *= inv2;
-		}
 		w *= wn;
 	}
 }
@@ -88,40 +91,29 @@ void ntt(vector<Field> &a, bool invert, Field wn){ //O(n log n)
 typedef vector<Long> polynomial;
 
 polynomial operator *(const polynomial &a, const polynomial &b) {
-
-	Long n = 1;
+	int n = 1;
 	vector<Field> fa(a.begin(), a.end());
 	vector<Field> fb(b.begin(), b.end());
-	
-	while(n < a.size() + b.size()){
-		n <<= 1;
-	}
+	while(n < a.size() + b.size()) n <<= 1;
 	fa.resize(n);
 	fb.resize(n);
 	
 	Field wn(fastPow(root , (MOD - 1) / n));
+	ntt(fa, wn);
+	ntt(fb, wn);
 	
-	ntt(fa, false , wn );
-	ntt(fb, false, wn );
-	
-	for(Long i = 0; i < n; i++){
+	for (int i = 0; i < n; i++) {
 		fa[i] *= fb[i];
+		fa[i] /= n;
 	}
 	wn = Field(fastPow(rootInv , (MOD - 1) / n));
-	ntt(fa, true, wn );
+	ntt(fa, wn);
 	
-	polynomial ans((Long)a.size() + (Long)b.size() - 1);
-	for(Long i = 0; i < ans.size(); i++){
-		ans[i] = fa[i].num;
-	}
+	polynomial ans((int)a.size() + (int)b.size() - 1);
+	for (int i = 0; i < ans.size(); i++) ans[i] = fa[i].val;
 	return ans;
 } 
 
-
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
 	return 0;
 }
