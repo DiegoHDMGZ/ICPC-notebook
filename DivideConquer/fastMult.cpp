96e9c7b49b0a52bc72544727085aebe0d2838d7f
Diego <dhurtadodemendoza@gmail.com>
2022-01-05 13:09:43 -0500

    simplify mod operations in fast mult

--- DivideConquer/fastMult.cpp
+++ DivideConquer/fastMult.cpp
@@ -7,17 +7,21 @@ using namespace std;
 
 typedef long long Long;
 
-Long add(Long a, Long b, Long mod) {
-	return (a + b ) % mod;
+const Long MOD = 1e9 + 7;
+Long add(Long a, Long b) {
+	if (a + b < MOD) return a + b;
+	return a + b - MOD;
 }
 
-Long fastMult(Long a, Long b, Long mod) {
+Long fastMult(Long a, Long b) {
+	a %= MOD;
+	b %= MOD;
 	Long ans = 0;
 	while (b > 0) {
 		if (b & 1) { //b % 2 == 1
-			ans = add(ans ,a , mod);
+			ans = add(ans, a);
 		}
-		a = add(a , a , mod);
+		a = add(a, a);
 		b >>= 1; //b /= 2
 	}
 	return ans;
