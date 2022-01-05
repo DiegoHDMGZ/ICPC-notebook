58b67c6aaebc6c6f97eda1d42990b10f5a4c0357
Diego <dhurtadodemendoza@gmail.com>
2022-01-05 13:16:51 -0500

    minor fix

--- dp/kadane.cpp
+++ dp/kadane.cpp
@@ -11,7 +11,7 @@ Long kadane(vector<Long> &v){
 	Long maxSoFar = -INF;
 	Long maxEndingHere = 0;
 	for (Long x : v ) {
-		maxEndingHere = max(x, maxEndingHere+ x);
+		maxEndingHere = max(x, maxEndingHere + x);
 		maxSoFar = max(maxSoFar, maxEndingHere);
 	}
 	return maxSoFar;
