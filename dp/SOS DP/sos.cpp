b84edc6896dacd79e5864aa7169bd7eb3ecfb216
Diego <dhurtadodemendoza@gmail.com>
2022-01-09 22:18:53 -0500

    add dp formulation in SOS dp

--- dp/SOS DP/sos.cpp
+++ dp/SOS DP/sos.cpp
@@ -13,6 +13,11 @@ int changeBit(int mask, int pos) {
 	return mask ^ (1 << pos);
 }
 
+//dp[mask][i] = sum over submasks that differs only in the first i bits
+//dp[mask][0] = A[mask]
+//dp[mask][i] = dp[mask][i - 1] , if (i - 1)-th bit off
+//dp[mask][i] = dp[mask][i - 1] + dp[mask xor 2^(i - 1)], if (i - 1)-th bit is on 
+
 //if subset == false, would be sum over supersets
 vector<Long> sumOverSubsets(vector<Long> &A, bool subset = true) { //O(n * 2^n)
 	//A have size 2^n
