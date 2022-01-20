65531bd6f40d055cb65b5abacfb6f41f08e93b53
Diego <dhurtadodemendoza@gmail.com>
2022-01-20 11:01:26 -0500

    big geometry refactoring

--- Math/Geometry/Basics/basics.cpp
+++ Math/Geometry/Basics/basics.cpp
@@ -1,465 +1,350 @@
 #include <bits/stdc++.h>
 #define debug(x) cout << #x << " = " << x << endl
 #define REP(i,n) for(Long i = 0; i < (Long)n; i++)
-#define pb push_back
 
 using namespace std;
 
-typedef long double Double;
 typedef long long Long;
+typedef long double Double;
 const Double EPS = 1e-10;
-const Double INF = 1e17;
+const Double PI = acos(-1);
+
+Double square(Double x) {
+	return x * x;
+}
+
+int getSgn(Double x) {
+	if (fabs(x) < EPS) return 0;
+	if (x > 0) return 1;
+	return -1;
+}
 
 struct Point{
-	Double x,y,z;
-	
-	Point(){
-		x = 0;
-		y = 0;
-		z = 0;
+	Double x, y;
+	Point(Double x = 0, Double y = 0): x(x), y(y) {}
+	Point operator +(const Point &other) const {
+		return Point(x + other.x, y + other.y);
 	}
-	Point(Double x, Double y, Double z = 0):
-		x(x), y(y), z(z){}
-
-	Point operator += (const Point &t){
-		x += t.x;
-		y += t.y;
-		z += t.z;
+	Point operator -(const Point &other) const {
+		return Point(x - other.x, y - other.y);
+	}
+	Point operator *(Double t) const {
+		return Point(x * t, y * t);
+	}
+	Point operator /(Double t) const {
+		assert(fabs(t) > EPS);
+		return Point(x / t, y / t);
+	}
+	Point operator +=(const Point &other) {
+		*this = *this + other;
 		return *this;
 	}
-	Point operator -= (const Point &t){
-		x -= t.x;
-		y -= t.y;
-		z -= t.z;
+	Point operator -=(const Point &other) {
+		*this = *this - other;
 		return *this;
 	}
-	Point operator *= (Double t){
-		x *= t;
-		y *= t;
-		z *= t;
+	Point operator *=(Double t) {
+		*this = *this * t;
 		return *this;
 	}
-	Point operator +(const Point &t) const {
-		return Point(*this) += t;
+	Point operator /=(Double t) {
+		*this = *this / t;
+		return *this;
 	}
-	Point operator - (const Point &t) const{
-		return Point(*this) -= t;
+	bool operator ==(const Point &P) const {
+		return fabs(x - P.x) < EPS && fabs(y - P.y) < EPS;
 	}
-	Point operator *(Double t) const{
-		return Point(*this) *= t;
+	Double abs2() {
+		return x * x + y * y;
 	}
-	
-	Point operator /(Double t) const{
-		return Point(*this) *= 1.0/t;
+	Double abs() {
+		return sqrt(abs2());
 	}
-	
-	bool operator == (const Point &P) const{
-		return fabs(x-P.x) < EPS && fabs(y-P.y) < EPS;
+	Point ort() {
+		return Point(-y, x);
 	}
-	
-	Double norm(){
-		return x * x + y * y + z * z;
+	Double dot(const Point &other) {
+		return x * other.x + y * other.y;
 	}
-	
-	Double abs(){
-		return sqrt(norm());
-	}
-	
-	Point ort(){
-		return Point(-y,x);
-	}
-	
 	Double cross(const Point &P) const {
 		return x * P.y - y * P.x;
 	}
-	
-	Double cross(const Point &a , const Point &b) const {
-		return (a - *this).cross(b - *this);
-	}
-	
-	bool inTriangle(Point A, Point B , Point C) {
-		//compare areas
-		Point P = Point(*this);
-		Double a1 = fabs(A.cross(B , C));
-		Double a2 = fabs(P.cross(A , B)) + fabs(P.cross(A , C)) + fabs(P.cross(B , C));
-		return fabs(a1 - a2) < EPS;
-	}
-	
-	bool inSegment(Point A, Point B) {
-		Point target = Point(*this);
-		if(fabs(A.cross(B , target)) < EPS) {
-			if(target.x >= min(A.x , B.x) && target.x <= max(A.x , B.x) && target.y >= min(A.y , B.y) && target.y <= max(A.y , B.y )) {
-				return true;
-			} 
-		}
-		return false;
-	}
-	
-	void debugPoint(string nombre){
-		cout << nombre << " = ( " << fixed << setprecision(2) <<  x << " , " << y << " , " << z << " ) " << endl; 
+	Double cross(const Point &A, const Point &B) const {
+		//Given the current point P, this calculate PA x PB
+		return (A - *this).cross(B - *this);
 	}
 };
 
-Long quad(Point P) {
-	if(P.x >= 0 && P.y > 0) {
-		return 1;
-	}
-	if(P.x < 0 && P.y >= 0) {
-		return 2;
-	}
-	if(P.x <= 0 && P.y < 0) {
-		return 3;
-	}
-	return 4;
-} 
+typedef Point Vector;
 
-bool counterClockwise(const Point &A, const Point &B , Point center) {
-	if(quad(A - center) != quad(B - center)) {
-		return quad(A - center) < quad(B - center);
-	}
-	
-	return center.cross(A, B) > 0;
-}
-
+//additional point operators with double
 Point operator *(Double a, Point b) {
 	return b * a;
 }
 
-Point operator/(Double a , Point b){
+Point operator /(Double a , Point b) {
 	return b / a;
 }
 
-double abs(Point A){
-	return sqrt(A.norm());
-}
-
-Double dot(Point A, Point B){
-	return A.x * B.x + A.y * B.y + A.z * B.z;
-}
-
-Point proj(Point A, Point B){
-	if(B.abs() < EPS){
-		return Point(INF,INF);
-	}
-	return  B* (dot(A,B)/B.abs() );
+//Auxiliary functions
+Double dist(Point A, Point B) {
+	return sqrt(square(B.x - A.x) + square(B.y - A.y));
 }
 
 Point midPoint(Point A, Point B) {
 	return Point((A + B) / 2.0);
 }
 
-Double angle(Point A, Point B){
-	if(A.abs()*B.abs() < EPS){
-		return INF;
+int quad(const Point &P) {
+	//quadrant of a point
+	if(P.x >= 0 && P.y > 0) return 1;
+	if(P.x < 0 && P.y >= 0) return 2;
+	if(P.x <= 0 && P.y < 0) return 3;
+	return 4;
+} 
+
+bool counterClockwise(const Point &A, const Point &B, Point &center) {
+	if (quad(A - center) != quad(B - center)) {
+		return quad(A - center) < quad(B - center);
 	}
-	return acos(dot(A,B) / (A.abs()*B.abs()) );
+	return center.cross(A, B) > 0;
 }
 
-Point cross(Point A, Point B){
-	return Point(A.y*B.z - A.z * B.y , 
-				 A.z*B.x - A.x * B.z ,
-				 A.x*B.y - A.y * B.x);
+//Vector Functions
+Vector getProj(Vector A, Vector B) {
+	//projection of A on to B
+	assert(B.abs() > EPS);
+	Vector unitB = B / B.abs();
+	return unitB * (A.dot(B)) / B.abs();
 }
 
-Double triple(Point A, Point B, Point C){
-	return dot(A,cross(B,C));
+Vector bisect(Vector A, Vector B) {
+	return A * B.abs() + B * A.abs();
 }
 
-Double dist(Point A, Point B){
-	return sqrt( (A.x-B.x)*(A.x-B.x) + (B.y-A.y)*(B.y-A.y) );
+Double getSimpleAngle(Vector A, Vector B) {
+	//return angle in [0 , PI] and does not have orientation
+	Double num = A.dot(B);
+	Double den = A.abs() * B.abs();
+	assert(fabs(den) >= EPS);
+	Double cosine = num / den;
+	if (cosine > 1 - EPS) return 0;
+	if (cosine < -1 + EPS) return PI;
+	return acos(num / den);
 }
 
-
-Point intersectPlanes(Point a1, Point n1, Point a2, Point n2, Point a3, Point n3){
-	Point x(n1.x, n2.x, n3.x);
-	Point y(n1.y, n2.y, n3.y);
-	Point z(n1.z, n2.z, n3.z); 
-	Point d(dot(a1, n1), dot(a2, n2), dot(a3, n3));
-	return Point(triple(d, y, z), 
-				triple(x, d, z), 
-				triple(x, y, d)) / triple(n1, n2, n3);
+Double getAngle(Vector A, Vector B) {
+	//return angle from A to B (order matters) in [0 , 2 * PI]
+	Double ang = getSimpleAngle(A, B);
+	int sineSgn = getSgn(A.cross(B));
+	if (sineSgn < 0) ang = 2 * PI - ang;
+	return ang;
 }
 
-Double areaDet(Point P1, Point P2, Point P3 ){
-	return (P2.x-P1.x)*(P3.y - P2.y) - (P3.x-P2.x)*(P2.y-P1.y);
+Vector rotate(Vector v, Double sine) {
+	Double cosine = sqrt(1 - sine * sine);
+	return Vector(v.x * cosine + v.y * sine, -v.x * sine + v.y * cosine);
 }
 
-long double area(Point P1, Point P2, Point P3 ){
-	return fabs(areaDet(P1,P2,P3))/2.0;
+//Segment functions
+bool between(Double x, Double l, Double r) {
+	//is x in [l , r] or [r, l] ?
+	return x >= min(l, r) - EPS && x <= max(l, r) + EPS;
 }
 
+bool axisHasIntersection(Double l1, Double r1, Double l2, Double r2) {
+	//return if it exists intersection in 
+	//[min(l1, r1), max(l1, r1)], [min(l2, r2), max(l2, r2)]
+	if (l1 > r1) swap(l1 , r1);
+	if (l2 > r2) swap(l2 , r2);
+	return max(l1, l2) <= min(r1, r2) + EPS;
+}
 
+struct Segment {
+	Point A, B;
+	Segment(){}
+	Segment(Point A, Point B): A(A), B(B){}
+	bool contains(const Point &P) const {
+		if (fabs(A.cross(B, P)) < EPS) {
+			return between(P.x, A.x, B.x) && between(P.y, A.y, B.y);
+		}
+		return false;
+	} 	
+	bool hasIntersection(const Segment &other) const {
+		Point C = other.A;
+		Point D = other.B;
+		if (C.cross(A, D) == 0 && C.cross(B, D) == 0) {
+			//same line
+			return axisHasIntersection(A.x, B.x, C.x, D.x) 
+			&& axisHasIntersection(A.y, B.y, C.y, D.y);
+		}
+		bool ok1 = getSgn(A.cross(B, C)) != getSgn(A.cross(B, D));
+		bool ok2 = getSgn(C.cross(D, A)) != getSgn(C.cross(D, B));
+		return ok1 && ok2;
+	}
+};
+
+//Line functions
 struct Line{
-	Double A , B ,C;
-	//AX + BY + C = 0
-	
-	Line(){	}
-	Line(Double a , Double b, Double c){
-		A = a;
-		B = b;
-		C = c;
-	}
-	
+	Double A, B, C; //Ax + By + C = 0
+	Line(){}
+	Line(Double A, Double B, Double C): A(A), B(B), C(C){}
 	Line(Point P1, Point P2){
 		A = P1.y - P2.y;
 		B = P2.x - P1.x;
-		C = -A*P1.x  - B*P1.y;	
+		C = -A * P1.x  - B * P1.y;	
 	}
-	
-	void debugLine(string nombre){
-		cout << nombre << " : " << fixed << setprecision(2) << A << "x + " << B << "y + " << C << " = 0" << endl;
+	Double evaluate(Point P) {
+		return A * P.x + B * P.y + C;
+	}
+	Double dist(const Point &P) {
+		//minimum distance from point to line
+		return fabs(evaluate(P)) / sqrt(square(A) + square(B));
 	}
 };
 
-Double dist(Line L, Point P){
-	//distancia minima de un punto a una recta
-	return fabs(L.A*P.x + L.B * P.y + L.C)/ sqrt(L.A*L.A + L.B * L.B);
+Double det(Double a, Double b, Double c, Double d) {
+	return a * d - b * c;
 }
 
-Double det(Double a, Double b, Double c, Double d){
-	//determinante
-	return a*d-b*c;
+vector<Point> intersect(Line &L1, Line &L2) {
+	//empty vector -> Parallel lines
+	//size 1 -> One point of intersection
+	//size 2 -> Equivalent Lines
+	Double den = det(L1.A, L1.B, L2.A, L2.B);
+	Double x = -det(L1.C, L1.B, L2.C, L2.B);
+	Double y = -det(L1.A, L1.C, L2.A, L2.C);
+	if (fabs(den) < EPS) {
+		if (fabs(x) < EPS && fabs(y) < EPS) return vector<Point>(2);
+		else return {};
+	}
+	Point ans(x, y);
+	ans /= den;
+	return {ans};
 }
 
+//Circle functions
 struct Circle{
-	Double r;
 	Point center;
+	Double r;
 	Circle(){}
-	Circle(Double rad, Point c){
-		r =rad;
-		center = c;
-	}
-	
-	Circle(Point c, Double rad){
-		r = rad;
-		center = c;
-	}
-	
-	void debugCircle(string nombre){
-		cout << nombre << " : " << "R = " << r << " , ";
-		center.debugPoint("Centro");
-	}
-	
+	Circle(Point center, Double r): center(center), r(r){}
 };
 
-Long sgn(Double x) {
-	if(x == 0) {
-		return 0;
+vector<Point> intersect(Line L, Circle C) {
+	//coordinate translation to center of circle
+	Double delta = C.center.x * L.A + C.center.y * L.B;
+	L.C += delta;
+	Double den = square(L.A) + square(L.B);
+	Double x0 = -L.A * L.C / den;
+	Double y0 = -L.B * L.C / den;
+	Double diff = square(C.r) - square(L.C) / den;
+	vector<Point> ans;
+	if(fabs(diff) < EPS){
+		//distance between line and center is equal to r
+		ans = {Point(x0, y0)};
+	} else if (diff < EPS){
+		//if the distance between the line and center 
+		//is greater than r, then there is no intersection
+		return {};
+	} else {
+		Double d = sqrt(diff / den);
+		ans.push_back(Point(x0 + L.B * d, y0 - L.A * d));
+		ans.push_back(Point(x0 - L.B * d, y0 + L.A * d));
 	}
-	if(x > 0) return 1;
-	return -1;
-}
-
-bool intersectAxis(Double a, Double b , Double c, Double d) {
-	if(a > b) {
-		swap(a , b);
-	}
-	if(c > d) {
-		swap(c , d);
-	}
-	return max(a , c) <= min(b , d);
-}
-
-bool intersectSegment(Point A, Point B, Point C, Point D) {
-	if(C.cross(A, D) == 0 && C.cross(B, D) == 0) {
-		//same line
-		return intersectAxis(A.x, B.x, C.x, D.x ) && intersectAxis(A.y ,B.y , C.y , D.y);
-	}
-	return sgn(A.cross(B, C)) != sgn(A.cross(B , D)) && sgn(C.cross(D, A)) != sgn(C.cross(D, B));
-}
-
-bool intersect(Point P1, Point d1, Point P2, Point d2, Point &resp ){
-	
-	//Cuando expresamos la recta como R: P = P0 + t a vectorial
-	
-	if(cross(d1,d2).abs() == 0){
-		return false;	
-	}
-	resp = P1 + (cross(P2-P1,d2).abs() / cross(d1,d2).abs() ) * d1;
-	return true;
-}
-
-int intersect(Line L1, Line L2, Point &resp){
-	//0 -> Paralelas
-	//1 -> Un punto intersección
-	//2 -> Rectas iguales
-	Double zn = det(L1.A,L1.B, L2.A, L2.B);
-	
-	if(fabs(zn) < EPS){
-		if(fabs(det(L1.C, L1.B , L2.C , L2.B)) < EPS && fabs(det(L1.A, L1.C , L2.A , L2.C)) < EPS){
-			return 2;
-		}
-		else{
-			return 0;
-		}
-	}
-	
-	resp.x = -det(L1.C, L1.B , L2.C , L2.B)/zn;
-	resp.y = -det(L1.A, L1.C , L2.A , L2.C)/zn;
-	
-
-	return 1;
-}
-
-int intersect(Line L, Circle C, Point &resp1, Point &resp2){
-/*
-	0 = no hay interseccion
-	1 = 1 punto
-	2 = 2 puntos
-*/
-
-//translación de coordenadas hacia C.centro como origen
-	Double plus = C.center.x*L.A + C.center.y*L.B;
-	L.C += plus;
-	
-	Double x0 = -L.A*L.C/(L.A*L.A + L.B * L.B);
-	Double y0 = -L.B * L.C / (L.A * L.A + L.B * L.B);
-	
-	if(L.C * L.C - C.r*C.r*(L.A*L.A+L.B*L.B) > EPS){
-		//si la distancia minima de la recta al origen es mayor que r, entonces no hay interseccion
-		return 0;
-	}
-	
-	if(fabs(L.C * L.C - C.r*C.r*(L.A*L.A+L.B*L.B)) < EPS){
-		//la distancia minima es casi igual al radio
-		resp1 = Point(x0 , y0 );
-		resp1 += C.center; //translación
-		resp2 = Point(INF,INF);
-		return 1;
-	}
-	
-	Double d = C.r*C.r - L.C*L.C / (L.A*L.A + L.B*L.B);
-	Double mult = sqrt(d/ (L.A * L.A + L.B * L.B));
-	
-	resp1 = Point(x0 + L.B*mult , y0 - L.A * mult);
-	resp2 = Point(x0 - L.B*mult , y0 + L.A * mult);
-	
-	resp1 += C.center; //translación
-	resp2 += C.center; //translación
-	
-	return 2;
-}
-
-int intersect(Circle C, Line L, Point &resp1 , Point &resp2){
-	return intersect(L,C,resp1,resp2);
-}
-
-int intersect(Circle C1, Circle C2, Point &resp1, Point &resp2){
-	//0 no hay interseccion
-	//1 una sola
-	//2 hay 2
-	//3 infintas
-	C2.center -= C1.center; //translacion
-	
-	Point origen = C1.center;
-	C1.center -= C1.center;
-	
-	//x² + y² = r1²
-	//(x-x2)² + (y-y2)² = r2²
-	// =>  x (-2x2) + y (-2y2) + (x2²+y2² + r1² - r2² ) =0
-	
-	Double d = dist(C1.center,C2.center);
-	if(d > C1.r + C2.r) return 0;
-	if(d < fabs(C1.r-C2.r)) return 0;
-	
-	Line L(-2*C2.center.x, -2*C2.center.y , C2.center.x * C2.center.x + C2.center.y * C2.center.y + C1.r * C1.r - C2.r * C2.r);
-	
-	if(C1.center == C2.center){
-		if(C1.r == C2.r) return 3;
-		else return 0;
-	}
-	
-	int x = intersect(C1,L,resp1,resp2);
-	
-	resp1 += origen;
-	resp2 += origen;
-	
-	return x;
-}
-
-Circle circuncenter(Point A, Point B , Point C) {
-	Point M = midPoint(A , B);
-	Point N = midPoint(A , C);
-	Point Mx = M + (B - A).ort();
-	Point Nx = N + (C - A).ort();
-	
-	Line L1(M , Mx);
-	Line L2(N , Nx);
-	
-	Circle ans;
-	intersect(L1 , L2, ans.center);
-	
-	ans.r = dist(ans.center , A);
+	//undo translation
+	for (Point &P : ans) P += C.center;
 	return ans;
 }
 
-Circle incenter(Point A , Point B, Point C) {
-	Double a = dist(B , C);
-	Double b = dist(A , C);
-	Double c = dist(A , B);
-	
-	Circle ans;
-	ans.center.x = (a * A.x + b * B.x + c * C.x ) / (a + b + c);
-	ans.center.y = (a * A.y + b * B.y + c * C.y ) / (a + b + c);
-	
-	Double p = (a + b + c) / 2.0;
-	
-	ans.r = sqrt(p * (p - a ) * (p - b) * (p - c)) / p;
+vector<Point> intersect(Circle C, Line L) {
+	return intersect(L, C);
+}
+
+vector<Point> intersect(Circle C1, Circle C2) {
+	//empty vector: no intersection
+	//size 3: infinite points
+	if (C1.center == C2.center) {
+		if (C1.r == C2.r) return vector<Point>(3);
+		else return {};
+	}
+	Double d = dist(C1.center, C2.center);
+	if(d > C1.r + C2.r + EPS) return {}; //too far away
+	if(d < fabs(C1.r - C2.r) - EPS) return {}; //circle inside the other
+	Point O(0 , 0);
+	C2.center -= C1.center; //translation
+	//Let (x', y') be the other center
+	//x^2 + y^2 = (r1)^2
+	//(x - x')^2 + (y - y')^2 = (r2)^2
+	//=> x (-2x') + y (-2y') + ((x')^2 + (y')^2 + (r1)^2 - (r2)^2) = 0
+	Double x2 = C2.center.x;
+	Double y2 = C2.center.y;
+	Double A = -2 * x2;
+	Double B = -2 * y2;
+	Double C = square(x2) + square(y2) + square(C1.r) - square(C2.r);
+	Line L(A, B, C);
+	vector<Point> ans = intersect(Circle(O, C1.r), L);
+	for (Point &P : ans) P += C1.center;
 	return ans;
 }
 
-Double area(vector<Point> &v) {
-	Double ans = 0;
-	Long sz = v.size();
-	for(Long i = 0; i < sz; i++) {
-		Point P = v[(i - 1 + sz) % sz ];
-		Point Q = v[i];
-		ans += (P.x - Q.x) * (P.y + Q.y);
+vector<Line> findTangents(Point P, Circle C) {
+	if (dist(P, C.center) < C.r - EPS) return {};
+	if (dist(P, C.center) < C.r + EPS) {
+		Point Q = P + (C.center - P).ort();
+		return {Line(P, Q)};
 	}
-	return fabs(ans) / 2.0;
+	Double D = dist(C.center, P);
+	Double R = C.r;
+	Vector PC = C.center - P;
+	Point A = rotate(PC, R / D) + P;
+	Point B = rotate(PC, -R / D) + P;
+	Line L1(P, B);
+	Line L2(P, A);
+	return {L1, L2};
 }
 
-
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-/*
-5 6
-7 -3
-2 3
-9
-
-20.9 3.5
-9 15.3
-2 3
-9
-
-11.4 3.5
-11.4 9
-2.4 3.6
-9
-*/
-	Point P1,P2;
-	Double r1,r2;
+//Triangle functions
+struct Triangle {
+	Point A, B, C;
+	Triangle(){}
+	Triangle(Point A, Point B, Point C): A(A), B(B), C(C){}
+	Double area() {
+		return fabs(A.cross(B, C)) / 2.0;
+	}
+	bool contains(Point &P) {
+		//compare areas
+		Double a1 = fabs(A.cross(B, C));
+		Double a2 = fabs(P.cross(A, B)) + fabs(P.cross(A, C)) + fabs(P.cross(B, C));
+		return fabs(a1 - a2) < EPS;
+	} 
 	
-	while(cin >> P1.x >> P1.y >> r1 >> P2.x >> P2.y >> r2){
-		Circle C1(P1,r1);
-		Circle C2(P2,r2);
-		
-		Point resp1,resp2;
-		int x = intersect(C1,C2,resp1,resp2);
-		if( x > 0){
-			resp1.debugPoint("resp1");
-			if(x == 2){
-				resp2.debugPoint("resp2");
-			}
-			else if (x == 3){
-				cout << "SUPERPUESTA" << endl;
-			}
-		}
-		else{
-			cout << "NO INTERSECTION" << endl;
-		}
+	Circle circumcircle() {
+		//assuming non-degenerated triangle
+		Point M = midPoint(A, B);
+		Point N = midPoint(A, C);
+		Point Mx = M + (B - A).ort();
+		Point Nx = N + (C - A).ort();
+		Line L1(M, Mx);
+		Line L2(N, Nx);
+		Circle ans;
+		ans.center = intersect(L1 , L2)[0];
+		ans.r = dist(ans.center , A);
+		return ans;
 	}
-	return 0;
-}
+
+	Circle incircle() {
+		//assuming non-degenerated triangle
+		Double a = dist(B, C);
+		Double b = dist(A, C);
+		Double c = dist(A, B);
+		Circle ans;
+		ans.center.x = (a * A.x + b * B.x + c * C.x);
+		ans.center.y = (a * A.y + b * B.y + c * C.y);
+		ans.center /= a + b + c;
+		Double p = (a + b + c) / 2.0;
+		ans.r = area() / p;
+		return ans;
+	}
+};
+
