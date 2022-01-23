8c9d7052fa627fee1f6940ac1006b7b19a6c9fb2
Diego <dhurtadodemendoza@gmail.com>
2022-01-23 18:03:26 -0500

    reduce descripotion of second D&C dp optimization

--- dp/Divide and Conquer DP/D&C-DP-optimized.cpp
+++ dp/Divide and Conquer DP/D&C-DP-optimized.cpp
@@ -7,24 +7,8 @@ typedef long long Long;
 
 /*
 Min cost array partition - Second optimization (similar to knuth's)
-dp[i][k]: Min cost partition for array [0 ... i] into k subarrays
-dp[i][k] = min{dp[p][k - 1] + cost(p + 1, r) , p < i}
-dp[i][1] = cost(0 , i)
+Quadrangle Inequality in cost also guarantees the following property:
 opt[i][k - 1] <= opt[i][k] <= opt[i + 1][k]
-A sufficient condition in cost is also Quadrangle Inequality:
-For all a < b < c < d
-cost(a, d) - cost(b, d) >= cost(a, c) - cost(b, c)  
-(For maximization is the opposite sign)
---------------------------------------------------
-Ways of proving that that cost satisfy Quadrangle Inequality:
-You can use induction in the following form
-cost(l, r) - cost(l + 1, r) >= cost(l, r - 1) - cost(l + 1, r - 1)
-Also, if cost(l, r) = f(S_l + ... + S_r) and all S_i are positive
-Then we can define cost(l, r) = f(A + x), cost(l + 1 , r) = f(A)
-cost(l, r - 1) = f(B + x) , cost(l + 1, r - 1) = f(B), A = B + y
-Notice that x = S_l and y = S_r
-So we want to prove f(A + x) - f(A) >= f(B + x) - f(B)
-Even more, if f''(x) >= 0, then the last inequality is satisfied
 */
 
 const int MX = 3000;
@@ -58,8 +42,4 @@ Long minCost(int n, int K) { //O(n^2)
 		}
 	}
 	return dp[n - 1][K];
-}
-
-int main() {
-	return 0;
 }
