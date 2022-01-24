c8d1754d2023bffbdc971af0c554cdf79338f35c
Diego <dhurtadodemendoza@gmail.com>
2022-01-23 23:17:31 -0500

    change some indices to int

--- DataStructure/SparseTable/sparseTable.cpp
+++ DataStructure/SparseTable/sparseTable.cpp
@@ -18,27 +18,27 @@ struct SparseTable {
 	}
 
 	void build(vector<Long> &A) { // O(|f| n log n)
-		Long n = A.size();
-		for (Long i = 0; i < n; i++) {
+		int n = A.size();
+		for (int i = 0; i < n; i++) {
 			st[i][0] = A[i];
 		}
 		
-		for (Long k = 1; k < LG; k++) {
-			for (Long i = 0; i + (1 << k) <= n; i++) {
+		for (int k = 1; k < LG; k++) {
+			for (int i = 0; i + (1 << k) <= n; i++) {
 				st[i][k] = f(st[i][k - 1], st[i + (1 << (k - 1))][k - 1]);
 			}
 		}
 	}
 	
-	/*Long query(Long l, Long r) { //O(|f| log n)
+	/*Long query(int l, int r) { //O(|f| log n)
 		Long ans = st[l][0];
 		if (l == r) return ans;
 		l++;
-		Long range = r - l + 1;
+		int range = r - l + 1;
 		
 		while ( range > 0) {
-			Long step = range & -range;
-			Long j = __builtin_ctz(step);
+			int step = range & -range;
+			int j = __builtin_ctz(step);
 			ans = f(ans , st[l][j]);
 			l += step;
 			range -= step;
@@ -47,14 +47,10 @@ struct SparseTable {
 		return ans;
 	}*/
 	
-	Long query(Long l, Long r) { //O(|f|)
+	Long query(int l, int r) { //O(|f|)
 		//special cases : idempotent(min, max, gcd)
-		Long sz = r - l + 1;
-		Long lg = 31 - (__builtin_clz(sz));
+		int sz = r - l + 1;
+		int lg = 31 - (__builtin_clz(sz));
 		return f(st[l][lg], st[r - (1 << lg) + 1][lg]);
 	}
 }st;
-
-int main() {
-	return 0;
-}
