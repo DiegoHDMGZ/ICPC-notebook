572e040f008d92dfd757434a5a29fdce3c5b6288
Diego <dhurtadodemendoza@gmail.com>
2022-01-26 22:00:01 -0500

    add Z complexity

--- Strings/Z/Z-function.cpp
+++ Strings/Z/Z-function.cpp
@@ -5,7 +5,7 @@ typedef long long Long;
 
 //Z[i] = the maximum length of a string that is a prefix and,
 //at the same time, a suffix that start at i
-vector<int> zFunction(string &s) {
+vector<int> zFunction(string &s) { //O(n)
 	int n = s.size();
 	vector<int> Z(n, 0);
 	Z[0] = n;
@@ -24,8 +24,4 @@ vector<int> zFunction(string &s) {
 		}
 	}
 	return Z;
-}
-
-int main() {
-	return 0;
 }
