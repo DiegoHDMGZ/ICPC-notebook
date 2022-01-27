acf5c1c94716af2a6dbc61719a0068c6fc33e1cc
Diego <dhurtadodemendoza@gmail.com>
2022-01-27 17:15:52 -0500

    fix style

--- Math/Number Theory/gcd/euclid.cpp
+++ Math/Number Theory/gcd/euclid.cpp
@@ -1,15 +1,11 @@
 #include <bits/stdc++.h>
-#define debug(x) cout << #x << " = " << x << endl
-#define REP(i,n) for(Long i = 0; i < (Long)n; i++)
-#define pb push_back
 using namespace std;
-
 typedef long long Long;
 
 //ax + by = gcd(a , b)
 //Property: the solutions given holds the following:
 //|x| <= |b / gcd(a, b)| , |y| <= |a / gcd(a, b)|
-Long gcd(Long a, Long b, Long & x, Long & y) { //O(min(loga,logb))
+Long gcd(Long a, Long b, Long &x, Long &y) { //O(min(loga,logb))
 	if(b == 0){
 		if (a >= 0) x = 1;
 		else x = -1;
@@ -22,9 +18,3 @@ Long gcd(Long a, Long b, Long & x, Long & y) { //O(min(loga,logb))
 	y = x2 - (a / b) * y2;
 	return g;
 }
-
-int main() {
-	return 0;
-}
-
-
