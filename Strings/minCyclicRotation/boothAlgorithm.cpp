1dc6d95b2303ae2d4079539c2d5761b5b1bb6c81
Diego <dhurtadodemendoza@gmail.com>
2022-01-26 21:13:08 -0500

    add booth algorithm

---
+++ Strings/minCyclicRotation/boothAlgorithm.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define debug(x) cout << #x << " = " << x << endl
+#define REP(i, n) for (Long i = 0; i < (Long)n; i++)
+using namespace std;
+
+typedef long long Long;
+
+//Booth Algorithm
+
+string minCyclicRotation(string s) { //O(n)
+	int n = s.size();
+	string t = s + s;
+	int opt = 0;
+	int i = 1;
+	while (i < n) {
+		for (int k = 0; k < n; k++) {
+			if (t[i + k] < t[opt + k]) {
+				opt = i;
+				break;
+			}
+			if (opt + k == i || t[opt + k] < t[i + k]) {
+				i += max(k, 1);
+				break;
+			}
+		}
+	}
+	rotate(s.begin(), s.begin() + opt, s.end());
+	return s;
+}
