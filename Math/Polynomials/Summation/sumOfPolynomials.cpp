6918f2109c1e59471570ea292854116f143d52a2
Diego <dhurtadodemendoza@gmail.com>
2022-02-04 23:31:40 -0500

    refactoring with ModInt

--- Math/Polynomials/Summation/sumOfPolynomials.cpp
+++ Math/Polynomials/Summation/sumOfPolynomials.cpp
@@ -4,48 +4,68 @@
 using namespace std;
 
 typedef long long Long;
-typedef vector<Long> poly;
+
+/*
+For a given polynomial, it computes the sum of evaluating the polynomial
+from x = 0 to x = n
+
+Usage:
+summation.build(polynomial)
+summation.query(n)
+*/
 
 const Long MOD = 1e9 + 7;
-
-Long mult(Long a, Long b) {
-	return (a * b ) % MOD;
-}
-
-Long add(Long a, Long b) {
-	return (a + b) % MOD;
-}
-
-Long sub(Long a, Long b) {
-	return (a - b + MOD) % MOD;
-}
-
-Long fastPow(Long a, Long b) { //O(logb)
-	Long ans = 1;
-	while (b > 0) {
-		if (b & 1) ans = mult(ans , a);
-		a = mult(a , a);
-		b >>= 1; 
+struct ModInt {
+	Long val;
+	ModInt(Long val = 0) {
+		val %= MOD;
+		if (val < 0) val += MOD;
+		this->val = val;
 	}
-	return ans;
-}
+	ModInt operator +(const ModInt &other) const {
+		if (val + other.val < MOD) return val + other.val;
+		return val + other.val - MOD;
+	}
+	ModInt operator *(const ModInt &other) const {
+		return (val * other.val) % MOD;
+	}
+	ModInt operator +=(const ModInt &other) {
+		*this = *this + other;
+		return *this;
+	}
+	ModInt operator *=(const ModInt &other) {
+		*this = *this * other;
+		return *this;
+	}
+	ModInt pow(Long b) const { //O(log b)
+		ModInt ans = 1;
+		ModInt a = val;
+		while (b > 0) {
+			if (b & 1) ans *= a;
+			a *= a;
+			b >>= 1;
+		}
+		return ans;
+	}
+	ModInt invert() const { //O(log mod) 
+		//mod prime
+		return pow(MOD - 2);
+	}
+	ModInt operator /(const ModInt &other) const {
+		return *this * other.invert();
+	}
+	ModInt operator /=(const ModInt &other) {
+		*this = *this / other;
+		return *this;
+	}
+};
 
-Long invert(Long a) { //O(log mod) , mod prime , (a , mod) coprimes
-	return fastPow(a, MOD - 2);
-}
+typedef vector<ModInt> poly;
 
-Long divide(Long a, Long b) {
-	return mult(a , invert(b));
-}
-
-poly operator +(const poly &A, const poly &B) {
-	poly ans(max(A.size(), B.size()));
-	for (Long i = 0; i < max(A.size(), B.size()); i++) {
-		Long a = 0;
-		if (i < A.size()) a = A[i];
-		Long b = 0;
-		if (i < B.size()) b = B[i];
-		ans[i] = add(a, b);
+poly operator +(const poly &a, const poly &b) {
+	poly ans(max(a.size(), b.size()), 0);
+	for(int i = 0; i < ans.size(); i++){
+		ans[i] = (i < a.size() ? a[i] : 0) + (i < b.size() ? b[i] : 0);
 	}
 	return ans;
 }
@@ -53,17 +73,17 @@ poly operator +(const poly &A, const poly &B) {
 const Long MX = 5002;
 
 struct Summation {
-	Long stirling[MX][MX];
+	ModInt stirling[MX][MX];
 	poly coef;
 	
-	void precalc() { //O(n^2)
+	void precalc() { //O(|poly|^2)
 		stirling[0][0] = 1;
-		for (Long n = 1; n < MX; n++) {
+		for (int n = 1; n < MX; n++) {
 			stirling[n][0] = 0;
 		}
-		for (Long n = 1; n < MX; n++) {
-			for (Long k = 1; k <= n; k++) {
-				stirling[n][k] = add(stirling[n - 1][k - 1] , mult(k , stirling[n - 1][k]));
+		for (int n = 1; n < MX; n++) {
+			for (int k = 1; k <= n; k++) {
+				stirling[n][k] = stirling[n - 1][k - 1] + stirling[n - 1][k] * k;
 			}
 		}
 	}
@@ -72,57 +92,34 @@ struct Summation {
 		precalc();
 	}
 
-	poly transformFalling(Long c, Long n) { //O(n)
+	poly transformFalling(ModInt c, int n) { //O(|poly|)
 		poly p(n + 1, 0);
 		for (Long i = 0; i <= n; i++) {
-			p[i] = mult(stirling[n][i], c);
+			p[i] = stirling[n][i] * c;
 		}
 		return p;
 	}
 	
-	void build(poly &p) { //O(n^2)
+	void build(poly &p) { //O(|poly|^2)
 		poly fallings;
-		for (Long i = 0; i < p.size(); i++) {
+		for (int i = 0; i < p.size(); i++) {
 			fallings = fallings + transformFalling(p[i], i);
 		}
-		coef = vector<Long>(fallings.size());
-		for (Long i = 0; i < fallings.size(); i++) {
-			coef[i] = divide(fallings[i], i + 1);
+		coef = vector<ModInt>(fallings.size());
+		for (int i = 0; i < fallings.size(); i++) {
+			coef[i] = fallings[i] / (i + 1);
 		}
 	}
 
-	Long query(Long x) {  //O(n)
-		x %= MOD;
-		Long ans = 0;
-		Long fact = 1;
-		Long val = x + 1;
+	ModInt query(Long n) {  //O(|poly|)
+		ModInt ans = 0;
+		ModInt fact = 1;
+		Long val = n + 1;
 		for (Long i = 0; i < coef.size(); i++) {
-			fact = mult(fact , val);
+			fact *= val;
 			val--;
-			ans = add(ans, mult(coef[i], fact)) ;
+			ans += coef[i] * fact;
 		}
 		return ans;
 	}
 }summation;
-
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	
-	Long n;
-	cin >> n;
-	poly p(n + 1);
-	for (Long i = n; i >= 0; i--) {
-		cin >> p[i];
-	}
-	summation.build(p);
-	while(true) {
-		Long x;
-		cin >> x;
-		if (x < 0) break;
-		cout << summation.query(x) << endl;
-	}
-
-	return 0;
-}
