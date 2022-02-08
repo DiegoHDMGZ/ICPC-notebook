8c13080a86b7fb377f9f98779b004674c72d37a3
Diego <dhurtadodemendoza@gmail.com>
2022-02-08 14:58:44 -0500

    replace pointers with edge index

--- Graph Theory/Flow/MaxFlow/Dinic/dinic-sparse.cpp
+++ Graph Theory/Flow/MaxFlow/Dinic/dinic-sparse.cpp
@@ -6,77 +6,67 @@ using namespace std;
 
 typedef long long Long;
 
-const Long MX = 5000;
+const int MX = 5000;
 const Long INF = 1e18;
 
 struct Edge{
-	Long to, cap, flow;
-	Edge *rev;
-	Edge(): rev(NULL) {}
-	Edge(Long to, Long cap) : to(to), cap(cap), flow(0), rev(NULL) {}
+	int from, to;
+	Long flow, cap;
+	int rev; //index of the backward edge in the adj list of to
+	Edge(int from, int to, Long cap, int rev): 
+		from(from), to(to), cap(cap), flow(0), rev(rev) {}
 };
 
 struct Graph{
-	vector<Edge*> adj[MX];
-	Long level[MX];
-	Long nextEdge[MX];
+	vector<Edge> adj[MX];  
+	int level[MX];
+	int nextEdge[MX];
 	
-	void clear(Long N = MX){
-		for(Long i = 0 ; i < N; i++){
+	void clear(int n) {
+		for (int i = 0; i < n; i++) {
 			adj[i].clear();
 			level[i] = -1;
 			nextEdge[i] = 0;
 		}
 	}
 	
-	void addEdge(Long u, Long v, Long w, bool dir){
-		Edge *forward = new Edge(v , w);
-		Edge *backward = new Edge(u , 0);
-		forward->rev = backward;
-		backward->rev = forward;
-
+	void addEdge(int u, int v, Long w, bool dir) {
+		Edge forward(u, v, w, adj[v].size());
+		Edge backward(v, u, 0, adj[u].size());
+		if (!dir) backward.cap = w;
 		adj[u].push_back(forward);
 		adj[v].push_back(backward);
-
-		if(!dir){
-			forward = new Edge(u , w);
-			backward = new Edge(v , 0);
-			forward->rev = backward;
-			backward->rev = forward;
-			adj[v].push_back(forward);
-			adj[u].push_back(backward);
-		}
 	}
 	
-	Long dfs(Long u, Long t ,Long f){ 
+	Long dfs(int u, int t ,Long f) { 
 		if(u == t) return f;
-		for(Long &i = nextEdge[u]; i < adj[u].size(); i++){
-			Edge *e = adj[u][i];
-			Long v = e->to;
-			Long cf = e->cap - e->flow;
-			if(cf == 0 || level[v] != level[u] + 1) continue;
-			
-			Long ret = dfs(v, t, min(f, cf) );
-			if(ret > 0){
-				e->flow += ret;
-				e->rev->flow -= ret;
+		for (int &i = nextEdge[u]; i < adj[u].size(); i++) {
+			Edge &e = adj[u][i];
+			int v = e.to;
+			Edge &rev = adj[v][e.rev];
+			Long cf = e.cap - e.flow;
+			if (cf == 0 || level[v] != level[u] + 1) continue;
+			Long ret = dfs(v, t, min(f, cf));
+			if (ret > 0) {
+				e.flow += ret;
+				rev.flow -= ret;
 				return ret;
 			}
 		}
 		return 0;
 	}
 	
-	bool bfs(Long s, Long t ){ //O(E)
-		deque<Long> q; 
+	bool bfs(int s, int t) { //O(E)
+		deque<int> q; 
 		q.push_back(s);
 		level[s] = 0;
-		while(!q.empty()){
-			Long u = q.front();
+		while (!q.empty()) {
+			int u = q.front();
 			q.pop_front();
-			for(Edge *e: adj[u]){
-				Long v = e->to;
-				Long cf = e->cap - e->flow;
-				if(level[v] == -1 && cf > 0){
+			for (Edge e : adj[u]) {
+				int v = e.to;
+				Long cf = e.cap - e.flow;
+				if (level[v] == -1 && cf > 0) {
 					level[v] = level[u] + 1;
 					q.push_back(v);
 				}
@@ -85,20 +75,22 @@ struct Graph{
 		return level[t] != -1;
 	}
 	
-	Long maxFlow(Long s, Long t, Long n){//General: O(E * V^2), Unit Cap: O(E * min(E^(1/2) , V^(2/3))), Unit Network: O(E * V^(1/2))
-		//unit network is a network in which all the edges have unit capacity,
-		//and for any vertex except s and t either incoming or outgoing edge is unique.
+	Long maxFlow(int s, int t, int n) {
+		//General: O(E * V^2)
+		//Unit Cap: O(E * min(E^(1/2) , V^(2/3)))
+		//Unit Network: O(E * V^(1/2))
+		//In unit network, all the edges have unit capacity
+		//and for any vertex except s and t either the 
+		//incoming or outgoing edge is unique.
 		Long ans = 0;
 		while(true){ //O(V) iterations
 			fill(level, level + n, -1);
-			if(!bfs(s, t) ){
-				break;
-			}
+			if (!bfs(s, t)) break;
 			//after bfs, the graph is a DAG
 			fill(nextEdge, nextEdge + n , 0);
 			Long inc;
 			do{
-				inc = dfs(s , t , INF);
+				inc = dfs(s, t, INF);
 				ans += inc;
 			} while (inc > 0);
 		}
