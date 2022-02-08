44190ecc9558553f8f6a4076631a58ba9be52329
Diego <dhurtadodemendoza@gmail.com>
2022-02-07 22:10:34 -0500

    add separated edges vector and handle reverse edges with index parity

--- Graph Theory/Flow/MaxFlow/fordFulkerson/fordFulkerson-small.cpp
+++ Graph Theory/Flow/MaxFlow/fordFulkerson/fordFulkerson-small.cpp
@@ -1,26 +1,23 @@
 #include <bits/stdc++.h>
-#define debug(x) cout << #x << " = " << x << endl
-#define REP(i,n) for(Long i = 0; i < (Long)n; i++)
-#define pb push_back
 using namespace std;
 
 typedef long long Long;
 
-const Long MX = 3000;
+const int MX = 3000;
 const Long INF = 1e18;
 
 struct Graph{
-	vector<Long> adj[MX];
+	vector<int> adj[MX];
 	Long cap[MX][MX]; 
 	Long flow[MX][MX];
 	bool vis[MX];
 	bool added[MX][MX];
 	
-	void clear(Long n) {
-		for (Long i = 0 ; i < n; i++) {
+	void clear(int n) {
+		for (int i = 0 ; i < n; i++) {
 			adj[i].clear();
 			vis[i] = false;
-			for (Long j = 0; j < n; j++) {
+			for (int j = 0; j < n; j++) {
 				cap[i][j] = 0;
 				flow[i][j] = 0;
 				added[i][j] = false;
@@ -28,23 +25,21 @@ struct Graph{
 		}
 	}
 	
-	void addEdge(Long u, Long v, Long w, bool dir) {
-		if (!added[min(u, v)][max(u , v)]) {
+	void addEdge(int u, int v, Long w, bool dir) {
+		if (!added[u][v]) {
 			adj[u].push_back(v);
 			adj[v].push_back(u);
+			added[u][v] = added[v][u] = true;
 		}
-		added[min(u , v)][max(u , v)] = true;
 		cap[u][v] += w;
-		if (!dir) {
-			cap[v][u] += w;
-		}
+		if (!dir) cap[v][u] += w;
 	}
 	
-	Long dfs(Long u, Long t ,Long f) { //O(E)
+	Long dfs(int u, int t, Long f) { //O(E)
 		if (u == t) return f;
 		if (vis[u]) return 0;
 		vis[u] = true;
-		for (Long v : adj[u]) {
+		for (int v : adj[u]) {
 			Long cf = cap[u][v] - flow[u][v];
 			if (cf == 0) continue;
 			Long ret = dfs(v, t, min(f, cf));
@@ -57,12 +52,12 @@ struct Graph{
 		return 0;
 	}
 	
-	Long maxFlow(Long s, Long t, Long n) { //O(E |F|) = O(E * nU)
+	Long maxFlow(int s, int t, int n) { //O(E |F|) = O(E * nU)
 		Long ans = 0;
-		while (true) { // O(|F|) iterations
+		while (true) { //O(|F|) iterations
 			fill(vis, vis + n, false);
 			Long inc = dfs(s, t, INF);
-			if(inc == 0) break;
+			if (inc == 0) break;
 			ans += inc;
 		}
 		return ans;
---
+++ Graph Theory/Flow/MaxFlow/fordFulkerson/fordFulkerson-sparse.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long Long;
+
+const int MX = 3000;
+const Long INF = 1e18;
+
+struct Edge{
+	int to;
+	Long flow, cap;
+	Edge(int to, Long cap) : to(to), cap(cap), flow(0) {}
+};
+
+struct Graph{
+	vector<Edge> edges;
+	vector<int> adj[MX];
+	bool vis[MX];
+	
+	void clear(int n) {
+		edges.clear();
+		for (int i = 0 ; i < n; i++) {
+			adj[i].clear();
+			vis[i] = false;
+		}
+	}
+	
+	void addEdge(int u, int v, Long w, bool dir) {
+		Edge forward(v, w);
+		Edge backward(u, 0);
+		adj[u].push_back(edges.size());
+		edges.push_back(forward);
+		adj[v].push_back(edges.size());
+		edges.push_back(backward);
+		if (!dir) addEdge(v, u, w, true);
+	}
+	
+	Long dfs(int u, int t, Long f) { //O(E)
+		if (u == t) return f;
+		if (vis[u]) return 0;
+		vis[u] = true;
+		for (int ind : adj[u]) {
+			Edge &e = edges[ind];
+			Edge &rev = edges[ind ^ 1];
+			int v = e.to;
+			Long cf = e.cap - e.flow;
+			if (cf == 0) continue;
+			Long ret = dfs(v, t, min(f, cf));
+			if (ret > 0) {
+				e.flow += ret;
+				rev.flow -= ret;
+				return ret;
+			}
+		}
+		return 0;
+	}
+	
+	Long maxFlow(int s, int t, int n) { //O(E |F|) = O(E * nU)
+		Long ans = 0;
+		while (true) { //O(|F|) iterations
+			fill(vis, vis + n, false);
+			Long inc = dfs(s, t, INF);
+			if (inc == 0) break;
+			ans += inc;
+		}
+		return ans;
+	}
+} G;
