a318dd0c9d78e9a4238246176c205fa93e2139e7
Diego <dhurtadodemendoza@gmail.com>
2022-02-10 15:14:29 -0500

    merge potentials sparse and dense

--- Graph Theory/Flow/MinCostFlow/potentials/potentials-small.cpp
+++ Graph Theory/Flow/MinCostFlow/potentials/potentials-small.cpp
@@ -1,14 +1,28 @@
 #include <bits/stdc++.h>
-#define debug(x) cout << #x << " = " << x << endl
-#define REP(i,n) for(Long i = 0; i < (Long)n; i++)
 using namespace std;
 
 typedef long long Long;
 
-//WARNING: No multi-edges allowed. No (u, v) (v, u) at the same time
+//This implementation is for small graphs (n <= 3000 aprox)
+//where O(n^2) memory will fit the memory limit.
+//So a 2D array can be used to speed up the algorithm a little bit.
+//WARNING: No multi-edges allowed. 
+//No antiparallel edges allowed i.e. No (u, v) (v, u) at the same time
 
-const int MX = 350;
-const Long INF = 1e9;
+const int MX = 2005;
+const Long INF = 1e18;
+
+struct Path{
+	int node;
+	Long weight;
+	Path(){}
+	Path(int node, Long weight) : 
+		node(node) , weight(weight) {}
+
+	bool operator >(const Path &P) const{
+		return weight > P.weight;
+	}
+};
 
 struct Graph{
 	vector<int> adj[MX];
@@ -20,7 +34,7 @@ struct Graph{
 	bool inQueue[MX];
 	
 	void clear(int n) {
-		for (int i = 0 ; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			adj[i].clear();
 			pot[i] = 0;
 			parent[i] = -1;
@@ -32,7 +46,6 @@ struct Graph{
 		}
 	}
 	
-	
 	void addEdge(int u, int v, Long w, Long c) {
 		adj[u].push_back(v);
 		adj[v].push_back(u);
@@ -41,7 +54,7 @@ struct Graph{
 		cost[v][u] = -c;
 	}
 	
-	void spfa(int s , int n) { //O(E V)
+	void spfa(int s, int n){ //O(E V)
 		for (int i = 0; i < n; i++) pot[i] = INF;
 		queue<int> q;
 		pot[s] = 0;
@@ -52,7 +65,7 @@ struct Graph{
 			q.pop();
 			inQueue[u] = false;
 			for (int v : adj[u]) {
-				if (cap[u][v] - flow[u][v] > 0 && pot[u] + cost[u][v] < pot[v]) {
+				if (cap[u][v] - flow[u][v] > 0 && pot[u] + cost[u][v] < pot[v]){
 					pot[v] = pot[u] + cost[u][v];
 					if (!inQueue[v]) q.push(v);
 					inQueue[v] = true;
@@ -61,7 +74,47 @@ struct Graph{
 		}
 	}
 	
+	pair<Long, Long> dijkstra(int s, int t, int n){ //O(E log V)
+		//<flow, cost>
+		priority_queue<Path, vector<Path>, greater<Path>> q;
+		vector<Long> d(n, INF);
+		vector<Long> residualCap(n, 0);
+		d[s] = 0;
+		residualCap[s] = INF;
+		q.push(Path(s, d[s]));
+		while (!q.empty()) {
+			Path p = q.top();
+			q.pop();
+			int u = p.node;
+			if (p.weight != d[u]) continue;
+			for (int v : adj[u]) {
+				Long cf = cap[u][v] - flow[u][v];
+				Long c = cost[u][v] + pot[u] - pot[v];
+				if (cf > 0 && d[u] + c < d[v]) {
+					assert(c >= 0);
+					d[v] = d[u] + c;
+					q.push(Path(v, d[v]));
+					residualCap[v] = min(residualCap[u], cf);
+					parent[v] = u;
+				}
+			}
+		}
+		if(d[t] == INF) return {0, 0};
+
+		for (int i = 0; i < n; i++) pot[i] += d[i];
+		Long cf = residualCap[t];
+		int cur = t;
+		while (true) {
+			flow[parent[cur]][cur] += cf;
+			flow[cur][parent[cur]] -= cf;
+			cur = parent[cur];
+			if (cur == s) break;
+		}
+		return {cf, pot[t] * cf};
+	}
 	
+	/*
+	//For dense graph, the quadratic version can be used
 	pair<Long, Long> dijkstra(int s, int t, int n) { //O(V^2)
 		//<flow, cost>
 		vector<Long> d(n , INF);
@@ -86,44 +139,23 @@ struct Graph{
 				}
 			}
 		}
-		
-		if (d[t] == INF) {
-			return {0, 0};
-		}
-		for (int i = 0; i < n; i++) {
-			pot[i] += d[i];
-		}
-		Long cf = residualCap[t];
-		
-		int cur = t;
-		while (true) {
-			flow[parent[cur]][cur] += cf;
-			flow[cur][parent[cur]] -= cf;
-			cur = parent[cur];
-			if (cur == s) break;
-		}
-		return {cf, pot[t] * cf};
+		//... (the same as the normal dijsktra here)
 	}
-	
+	*/
 	
 	pair<Long, Long> minCostFlow(int s, int t, int n) { 
-		//O(V^2 * maxFlow)
+		//O(E log V *  maxFlow)
 		//maxFlow <= V * U, where U is the maximum capacity
 		//Initially no negative cycles
 		//<maxFlow, minCost>
-		spfa(s  , n); //not necessary if there is no negative edges
+		spfa(s, n); //not necessary if there is no negative edges
 		pair<Long, Long> inc;
 		pair<Long, Long> ans = {0, 0};
 		do {
-			inc = dijkstra(s, t , n);
+			inc = dijkstra(s, t, n);
 			ans.first += inc.first;
 			ans.second += inc.second;
 		} while (inc.first > 0);
-		
 		return ans;
 	}
 } G;
-
-int main() {
-	return 0;
-}
