b351a185c34934a68064dcf4ff808e54506164c0
Diego <dhurtadodemendoza@gmail.com>
2022-02-10 19:15:29 -0500

    max flow min cost refactoring

--- Graph Theory/Flow/MinCostFlow/successive-shortest-paths/successive-shortest-paths.cpp
+++ Graph Theory/Flow/MinCostFlow/successive-shortest-paths/successive-shortest-paths.cpp
@@ -8,108 +8,90 @@ typedef long long Long;
 const Long MX = 5000;
 const Long INF = 1e18;
 
+typedef pair<Long, Long> Pair;
+
 struct Edge{
-	Long from , to, cap, flow, cost;
-	Edge *rev;
-	Edge(): rev(NULL) {}
-	Edge(Long from , Long to, Long cap, Long cost) : from(from) , to(to), cap(cap), flow(0), cost(cost), rev(NULL) {}
+	int from, to;
+	Long cap, flow, cost;
+	int rev; //index of the backward edge in the adj list of to
+	Edge() {}
+	Edge(int from, int to, Long cap, Long cost, int rev) : 
+		from(from), to(to), cap(cap), flow(0), cost(cost), rev(rev){}
 };
 
 struct Graph{
-	vector<Edge*> adj[MX];
-	Edge* parent[MX];
-	vector<Edge*> E;
+	vector<Edge> adj[MX];
+	vector<Edge> edges;
+	int parentEdge[MX];
 	
 	void clear(int n) {
-		for (Long i = 0 ; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			adj[i].clear();
-			parent[i] = NULL;
 		}
-		E.clear();
+		edges.clear();
 	}
 	
-	void addEdge(Long u, Long v, Long w, Long cost, bool dir) {
-		Edge *forward = new Edge(u , v , w, cost);
-		Edge *backward = new Edge(v , u , 0, -cost);
-		forward->rev = backward;
-		backward->rev = forward;
-
+	void addEdge(int u, int v, Long w, Long cost, bool dir) {
+		Edge forward(u, v, w, cost, adj[v].size());
+		Edge backward(v, u, 0, -cost, adj[u].size());
 		adj[u].push_back(forward);
 		adj[v].push_back(backward);
-		E.push_back(forward);
-		E.push_back(backward);
-		
-		if(!dir){
-			forward = new Edge(v , u , w, cost);
-			backward = new Edge(u , v , 0, -cost);
-			forward->rev = backward;
-			backward->rev = forward;
-			adj[v].push_back(forward);
-			adj[u].push_back(backward);
-			E.push_back(forward);
-			E.push_back(backward);
+		edges.push_back(forward);
+		edges.push_back(backward);
+		if (!dir) addEdge(v, u, w, cost, true);
+	}
+	
+	void pushFlow(int s, int t, Long inc) {
+		int v = t;
+		while (v != s) {
+			Edge &backward = adj[v][parentEdge[v]];
+			int u = backward.to;
+			Edge &forward = adj[u][backward.rev];
+			forward.flow += inc;
+			backward.flow -= inc;
+			v = u;
 		}
 	}
 	
-	pair<Long, Long> spfa(int s , int t, int n) { //O(E V)
+	Pair spfa(int s, int t, int n) { //O(E V)
+		//<flow, cost>
 		vector<Long> d(n, INF);
-		vector<Long> cnt(n , 0);
-		vector<bool> inQueue(n , false);
-		d[s] = 0;
+		vector<bool> inQueue(n, false);
+		vector<Long> residualCap(n, 0);
 		queue<int> q;
+		d[s] = 0;
+		residualCap[s] = INF;
 		inQueue[s] = true;
 		q.push(s);
 		while (!q.empty()) {
-			Long u = q.front();
+			int u = q.front();
 			q.pop();
 			inQueue[u] = false;
-			cnt[u]++;
-			assert(cnt[u] < n);
-			for (Edge *e : adj[u]) {
-				Long v = e->to;
-				if (e->cap - e->flow > 0 && d[u] + e->cost < d[v]){
-					d[v] = d[u] + e->cost;
-					if(!inQueue[v]){
-						q.push(v);
-					}
-					parent[v] = e;
+			for (Edge e : adj[u]) {
+				int v = e.to;
+				Long cf = e.cap - e.flow;
+				if (cf > 0 && d[u] + e.cost < d[v]){
+					d[v] = d[u] + e.cost;
+					if (!inQueue[v]) q.push(v);
+					parentEdge[v] = e.rev;
+					residualCap[v] = min(residualCap[u], cf);
 					inQueue[v] = true;
 				}
 			}
 		}
-		
-		if (d[t] == INF) return {0,0};
-		
-		Long cf = INF;
-		int cur = t;
-		while (true) {
-			cf = min(cf , parent[cur]->cap - parent[cur]->flow);
-			cur = parent[cur]->from;
-			if (cur == s) {
-				break;
-			}
-		}
-		cur = t;
-		Long cost = 0;
-		while (true) {
-			cost += cf * parent[cur]->cost;
-			parent[cur]->flow += cf;
-			parent[cur]->rev->flow -= cf;
-			cur = parent[cur]->from;
-			if (cur == s) {
-				break;
-			}
-		}
-		return {cf, cost};
+		if (d[t] == INF) return {0, 0};
+		Long cf = residualCap[t];
+		pushFlow(s, t, cf);
+		return {cf, d[t] * cf};
 	}
 	
-	pair<Long, Long> minCostFlow(int s, int t, int n) { 
+	Pair minCostFlow(int s, int t, int n) { 
 		//O(E * V * maxFlow )
 		//maxFlow <= V * U, where U is the maximum capacity
-		//Initially no negative cycles
+		//Assumption: Initially no negative cycles
 		//<maxFlow, minCost>
-		pair<Long,Long> inc;
-		pair<Long, Long> ans = {0, 0};
+		Pair inc;
+		Pair ans = {0, 0};
 		do {
 			inc = spfa(s, t, n);
 			ans.first += inc.first;
@@ -118,7 +100,3 @@ struct Graph{
 		return ans;
 	}
 } G;
-
-int main() {
-	return 0;
-}
