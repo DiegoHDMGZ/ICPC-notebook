d7b73bf4e1b9c5c5118db18ccb3d2b3a86a63ae2
Diego <dhurtadodemendoza@gmail.com>
2022-02-13 11:20:24 -0500

    fix style

--- DataStructure/SegmentTree/with STL/lowerBoundInRange.cpp
+++ DataStructure/SegmentTree/with STL/lowerBoundInRange.cpp
@@ -1,7 +1,4 @@
 #include <bits/stdc++.h>
-#define debug(x) cout << #x << " = " << x << endl
-#define REP(i, n) for (Long i = 0; i < (Long)n; i++)
-#define pb push_back
 using namespace std;
 
 typedef long long Long;
