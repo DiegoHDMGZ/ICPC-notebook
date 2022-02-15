3586468526d04dea327f87d77567116f9780788d
Diego <dhurtadodemendoza@gmail.com>
2022-02-14 19:55:52 -0500

    ModInt refactoring

--- Math/Number Theory/Lucas/lucas.cpp
+++ Math/Number Theory/Lucas/lucas.cpp
@@ -1,8 +1,4 @@
 #include <bits/stdc++.h>
-#define debug(x) cout << #x << " = " << x << endl
-#define REP(i,n) for(Long i = 0; i < (Long)n; i++)
-#define pb push_back
-
 using namespace std;
 
 typedef long long Long;
@@ -12,53 +8,76 @@ typedef long long Long;
 //C(m , n) = Prod (mi, ni) (mod p)
 //where mi , ni are the p-expansions of m and n respectively.
 
-const Long MX = 100;
-Long fact[MX];
-Long inv[MX];
+Long MOD;
 
-Long mult(Long a, Long b, Long mod) {
-	return (a * b) % mod;
-}
-
-Long fastPow(Long a, Long b , Long mod) { //O(log b)
-	Long ans = 1;
-	while (b > 0) {
-		if(b & 1) ans = mult(ans ,a , mod);
-		a = mult(a , a  , mod);
-		b >>= 1;
+struct ModInt {
+	Long val;
+	ModInt(Long val = 0) {
+		this->val = val;
 	}
-	return ans;
-}
+	ModInt operator +(const ModInt &other) const {
+		if (val + other.val < MOD) return val + other.val;
+		return val + other.val - MOD;
+	}
+	ModInt operator -(const ModInt &other) const {
+		if (val - other.val >= 0) return val - other.val;
+		return val - other.val + MOD;
+	}
+	ModInt operator *(const ModInt &other) const {
+		return (val * other.val) % MOD;
+	}
+	ModInt operator *=(const ModInt &other) {
+		*this = *this * other;
+		return *this;
+	}
+	ModInt pow(Long b) const { //O(log b)
+		ModInt ans = 1;
+		ModInt a = val;
+		while (b > 0) {
+			if (b & 1) ans *= a;
+			a *= a;
+			b >>= 1;
+		}
+		return ans;
+	}
+	ModInt invert() const { //O(log mod) 
+		//mod prime
+		return pow(MOD - 2);
+	}
+	ModInt operator /(const ModInt &other) const {
+		return *this * other.invert();
+	}
+	ModInt operator /=(const ModInt &other) {
+		*this = *this / other;
+		return *this;
+	}
+};
 
-Long invert(Long a, Long m) { //O(log m) , m prime , a , m coprimes
-	return fastPow(a, m - 2, m);
-}
+const int MX = 1e6;
+ModInt fact[MX];
+ModInt inv[MX];
 
 void init(Long mod) { //O(mod)
+	MOD = mod;
 	fact[0] = 1;
-	inv[0] = 1;
-	for (int i = 1; i < mod; i++) {
-		fact[i] = mult(fact[i-1], i, mod);
-	}
-	inv[mod - 1] = invert(fact[mod - 1], mod);
-	for (int i = mod - 2; i >= 0; i--) {
-		inv[i] = mult(inv[i + 1], i + 1, mod);
-	}
+	for (int i = 1; i < MOD; i++) fact[i] = fact[i - 1] * i;
+	inv[MOD - 1] = fact[MOD - 1].invert();
+	for (int i = MOD - 2; i >= 0; i--) inv[i] = inv[i + 1] * (i + 1);
 }
 
-Long comb(Long N, Long M, Long mod) { //O(1)
-	if(N < M) return 0;
-	return mult(mult(fact[N], inv[M], mod), inv[N - M], mod); 
+ModInt comb(Long n, Long m) { //O(1)
+	if (n < m) return 0;
+	return fact[n] * inv[m] * inv[n - m]; 
 }
 
 //call init(MOD) before
-Long combLucas(Long N, Long M, Long mod) { //O(log N + log M)
+ModInt combLucas(Long n, Long m) { //O(log N + log M)
 	//mod is prime
-	Long ans = 1;
-	while (N > 0 || M > 0) {
-		ans = mult(ans, comb(N % mod, M % mod , mod), mod);
-		N /= mod;
-		M /= mod; 
+	ModInt ans = 1;
+	while (n > 0 || m > 0) {
+		ans *= comb(n % MOD, m % MOD);
+		n /= MOD;
+		m /= MOD; 
 	}
 	return ans;
 }
