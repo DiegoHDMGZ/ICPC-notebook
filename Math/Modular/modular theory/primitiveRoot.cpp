0741aaa4e823486703ae778799210671ee894171
Diego <dhurtadodemendoza@gmail.com>
2022-02-15 01:10:40 -0500

    fix style

--- Math/Modular/modular theory/primitiveRoot.cpp
+++ Math/Modular/modular theory/primitiveRoot.cpp
@@ -9,25 +9,20 @@ typedef long long Long;
 //such that gcd(a, n) = 1, there exists an integer k such that:
 //(g ^ k) = a (mod n).
 
-Long mult(Long a, Long b, Long mod) {
-	a %= mod;
-	b %= mod;
-	return (a * b) % mod;
-}
-
 Long fastPow(Long a, Long b, Long mod) { //O(log b)
 	Long ans = 1;
 	while (b > 0) {
-		if (b & 1) ans = mult(ans, a, mod);
-		a = mult(a, a, mod);
+		if (b & 1) ans = (ans * a) % mod;
+		a *= a;
+		a %= mod;
 		b >>= 1;
 	}
 	return ans;
 }
 
-Long primitiveRoot(Long mod) { //O( mod log mod log phi(mod) + sqrt(mod))  
+Long primitiveRoot(Long mod) { //O(mod log mod log phi(mod) + sqrt(mod))  
 	Long phi = mod - 1; // phi = euler(mod) for not prime mods
-	//if( mod < 1e7 ) we can use extended Sieve to factorize
+	//if (mod < 1e7) we can use extended Sieve to factorize
 	Long n = phi;
 	Long i = 2;
 	vector<Long> v;
@@ -39,7 +34,7 @@ Long primitiveRoot(Long mod) { //O( mod log mod log phi(mod) + sqrt(mod))
 		i++;
 	}
 	if(n > 1) v.push_back(phi / n);
-	for(Long ans = 2; ans <= mod; ans++) {
+	for (Long ans = 2; ans <= mod; ans++) {
 		bool ok = true;
 		REP(i, v.size()) {
 			if (fastPow(ans, v[i], mod) == 1) {
