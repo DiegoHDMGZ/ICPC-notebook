d68055565e05511a30164057a22e28f4574f5f89
Diego <dhurtadodemendoza@gmail.com>
2022-02-15 18:15:02 -0500

    rename variables

--- Math/Modular/modular theory/discreteRoot.cpp
+++ Math/Modular/modular theory/discreteRoot.cpp
@@ -5,21 +5,21 @@ using namespace std;
 
 typedef long long Long;
 
-// (x ^ e) % mod  = rem, mod prime 
+// (x ^ k) % mod  = rem, mod prime 
 // Use discreteLog and primiteRoot files
 
 Long fastPow(Long a, Long b, Long mod);
 Long primitiveRoot(Long mod);
 Long discreteLog(Long base, Long rem, Long mod);
 
-Long discreteRoot(Long e, Long rem, Long mod) {
+Long discreteRoot(Long k, Long rem, Long mod) {
 	if (rem == 0) {
 		//Assumption: 0^0 = 1
-		if (e != 0 || mod == 1) return 0;
+		if (k != 0 || mod == 1) return 0;
 		else return -1;
 	}
-	Long g = primitiveRoot(mod);
-	Long y = discreteLog(fastPow(g, e, mod), rem, mod);
+	Long root = primitiveRoot(mod);
+	Long y = discreteLog(fastPow(root, k, mod), rem, mod);
 	if (y == -1) return -1;
-	return fastPow(g, y, mod);
+	return fastPow(root, y, mod);
 }
