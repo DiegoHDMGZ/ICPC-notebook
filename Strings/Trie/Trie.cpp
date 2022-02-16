16c2ea978018603a48d7b9a7e3a047bf0e55dcb8
Diego <dhurtadodemendoza@gmail.com>
2022-02-16 01:25:43 -0500

    small refactoring in trie functions

--- Strings/Trie/Trie.cpp
+++ Strings/Trie/Trie.cpp
@@ -1,22 +1,19 @@
 #include <bits/stdc++.h>
-#define debug(x) cout << #x << " = " << x << endl
-#define REP(i,n) for(Long i = 0; i < (Long)n; i++)
-#define pb push_back
 using namespace std;
 
 typedef long long Long;
 
-const Long MX = 1e5; //sum of length of all words + 1 
-const Long ALPH = 26; // alphabet size
+const int MX = 1e5; //sum of length of all words + 1 
+const int ALPH = 26; // alphabet size
 
 struct Trie{
-	Long memo[MX][ALPH];
-	bool term[MX]; //terminal node
-	Long nodes = 1;
+	int memo[MX][ALPH];
+	bool terminal[MX];
+	int nodes = 1;
 	
 	void clear() {
-		for (Long i = 0; i < nodes; i++) {
-			term[i] = 0;
+		for (int i = 0; i < nodes; i++) {
+			terminal[i] = 0;
 			for (int j = 0; j < ALPH; j++) {
 				memo[i][j] = 0;
 			}
@@ -24,47 +21,33 @@ struct Trie{
 		nodes = 1;
 	}
 	
-	void addWord(string &s) { //O(n)
+	void addWord(string &s) { //O(|s|)
 		int n = s.size();
 		int u = 0;
-		for (Long i = 0; i < n; i++) {
-			Long c = s[i] - 'a';
-			if (memo[u][c] == 0) {
-				memo[u][c] = nodes++;
-			}
+		for (int i = 0; i < n; i++) {
+			int c = s[i] - 'a';
+			if (memo[u][c] == 0) memo[u][c] = nodes++;
 			u = memo[u][c];
 		}
-		term[u] = true;
+		terminal[u] = true;
 	}
 	
-	bool isPrefix(string &s) { //O(n)
-		Long n = s.size();
-		Long u = 0;
-		for (Long i = 0; i < n; i++) {
-			Long c = s[i] - 'a';
-			if (memo[u][c] == 0) {
-				return false;
-			}
+	int feed(string &s) { //O(|s|)
+		int n = s.size();
+		int u = 0;
+		for (int i = 0; i < n; i++) {
+			int c = s[i] - 'a';
+			if (memo[u][c] == 0) return -1;
 			u = memo[u][c];
 		}
-		return true;
+		return u;
+	}
+	bool isPrefix(string &s) { //O(|s|)
+		return feed(s) != -1;
 	}
 	
-	bool isWord(string &s) { //O(n)
-		Long n = s.size();
-		Long u = 0;
-		
-		for (Long i = 0; i < n; i++) {
-			Long c = s[i] - 'a';
-			if (memo[u][c] == 0) {
-				return false;
-			}
-			u = memo[u][c];
-		}
-		return term[u]; 
+	bool isWord(string &s) { //O(|s|)
+		int u = feed(s);
+		return u != -1 && terminal[u];
 	}
-}trie;
-
-int main() {
-	return 0;
-}
+} trie;
