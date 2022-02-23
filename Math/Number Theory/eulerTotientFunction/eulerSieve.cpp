f884e0f7dc5d477e1903f094ec379493a900bd48
Diego <dhurtadodemendoza@gmail.com>
2022-02-23 18:45:08 -0500

    fix style

--- Math/Number Theory/eulerTotientFunction/eulerSieve.cpp
+++ Math/Number Theory/eulerTotientFunction/eulerSieve.cpp
@@ -9,14 +9,10 @@ const int MX = 1e6;
 Long phi[MX];
 
 void calculate() { //O(n log log n)
-	for (int i = 0; i < MX; i++) {
-		phi[i] = i;
-	}
+	for (int i = 0; i < MX; i++) phi[i] = i;
 	for (int i = 2; i < MX; i++) {
 		if (phi[i] == i) {
-			for (int j = i; j < MX; j += i) {
-				phi[j] -= phi[j] / i;
-			}
+			for (int j = i; j < MX; j += i) phi[j] -= phi[j] / i;
 		}
 	}
 }
