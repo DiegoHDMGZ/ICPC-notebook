398311044704cf4e6a70296eee40571a80bbf65f
Diego <dhurtadodemendoza@gmail.com>
2022-02-25 18:32:48 -0500

    delete unnecesary space

--- Miscellaneous/random.cpp
+++ Miscellaneous/random.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 typedef long long Long;
 
-mt19937_64  rng(chrono::steady_clock::now().time_since_epoch().count());
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 Long random(Long a, Long b) {
 	return uniform_int_distribution<Long>(a , b)(rng);
