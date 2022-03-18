b99ffd1e08814743910ba18c3da39d19951268cd
Diego <dhurtadodemendoza@gmail.com>
2022-03-17 23:15:16 -0500

    replace deque by queue

--- Graph Theory/Flow/Dilworth Theorem/dilworth.cpp
+++ Graph Theory/Flow/Dilworth Theorem/dilworth.cpp
@@ -37,23 +37,23 @@ struct Graph{
 		//We build all the transitive edges using toposort
 		int s = 0;
 		int t = 2 * n + 1;
-		deque<int> q;
+		queue<int> q;
 		for (int u = 0; u < n; u++) {
-			if (indegree[u] == 0) q.push_back(u);
+			if (indegree[u] == 0) q.push(u);
 		}
 		int cnt = 0;
 		while (!q.empty()){
 			int u = q.front();
+			q.pop();
 			cnt++;
 			GFlow.addEdge(s, u + 1, 1, true);
 			GFlow.addEdge(u + 1 + n, t, 1, true);
-			q.pop_front();
 			for (int p : parents[u]) {
 				GFlow.addEdge(p + 1, u + 1 + n, 1, true);
 			}
 			for (int v : adj[u]) {
 				indegree[v]--;
-				if (indegree[v] == 0) q.push_back(v);
+				if (indegree[v] == 0) q.push(v);
 				for (int p : parents[u]) parents[v].insert(p);
 				parents[v].insert(u);
 			}
