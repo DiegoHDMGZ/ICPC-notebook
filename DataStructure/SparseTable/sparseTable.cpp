075c3837105b98c09c5d9a37a93050cda6fa2856
Diego <dhurtadodemendoza@gmail.com>
2022-03-21 12:51:07 -0500

    cache optimization in sparse table

--- DataStructure/SparseTable/sparseTable.cpp
+++ DataStructure/SparseTable/sparseTable.cpp
@@ -1,55 +1,31 @@
 #include <bits/stdc++.h>
-#define debug(x) cout << #x << " = " << x << endl
-#define REP(i,n) for(Long i = 0; i < (Long)n; i++)
-
 using namespace std;
 
 typedef long long Long;
 
 const int MX = 1e6;
-const int LG = 32 - __builtin_clz(MX);
+const int LG = __lg(MX) + 1;
 
 struct SparseTable {
-	Long st[MX][LG];
+	Long st[LG][MX];
 	
 	Long f(Long a, Long b) {
-		return min(a , b);
+		return min(a, b);
 	}
 
 	void build(vector<Long> &A) { // O(|f| n log n)
 		int n = A.size();
-		for (int i = 0; i < n; i++) {
-			st[i][0] = A[i];
-		}
-		
+		for (int i = 0; i < n; i++) st[0][i] = A[i];
 		for (int k = 1; k < LG; k++) {
 			for (int i = 0; i + (1 << k) <= n; i++) {
-				st[i][k] = f(st[i][k - 1], st[i + (1 << (k - 1))][k - 1]);
+				st[k][i] = f(st[k - 1][i], st[k - 1][i + (1 << (k - 1))]);
 			}
 		}
 	}
 	
-	/*Long query(int l, int r) { //O(|f| log n)
-		Long ans = st[l][0];
-		if (l == r) return ans;
-		l++;
-		int range = r - l + 1;
-		
-		while ( range > 0) {
-			int step = range & -range;
-			int j = __builtin_ctz(step);
-			ans = f(ans , st[l][j]);
-			l += step;
-			range -= step;
-		}
-		
-		return ans;
-	}*/
-	
 	Long query(int l, int r) { //O(|f|)
 		//special cases : idempotent(min, max, gcd)
-		int sz = r - l + 1;
-		int lg = 31 - __builtin_clz(sz);
-		return f(st[l][lg], st[r - (1 << lg) + 1][lg]);
+		int lg = __lg(r - l + 1);
+		return f(st[lg][l], st[lg][r - (1 << lg) + 1]);
 	}
-}st;
+} st;
