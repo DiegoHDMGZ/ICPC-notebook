06cb9010e86f98ed35331f28c74b0b2a634b3c66
Diego <dhurtadodemendoza@gmail.com>
2022-03-24 16:12:35 -0500

    treap factoring

--- DataStructure/Treap/treap.cpp
+++ DataStructure/Treap/treap.cpp
@@ -1,147 +1,120 @@
 #include <bits/stdc++.h>
-#define debug(x) cout << #x << " = " << x << endl
-#define REP(i,n) for(Long i = 0; i < (Long)n; i++)
-#define pb push_back
-
 using namespace std;
-
 typedef long long Long;
 
-const int MAXINT = 1e9;
+/*
+Every node T has two main values (key, priority) and two 
+nullable children. Assume that all keys are different. 
+Priorities also must be different with high probability using a
+big enough pool of random numbers (preferable in the order of n^2)
+If a node T has values (X, Y) then all the nodes
+in the subtree of T have Y' <= Y, all nodes in the left subtree
+have X' < X and finally all nodes in the right subtree have X' > X
 
-mt19937_64  rng(chrono::steady_clock::now().time_since_epoch().count());
+The height of the tree will be O(log n) with high probability if 
+the elements are distinct. If there are equal elements we can enforce
+this rule by transforming each element into a pair <value, frequency>.
+The implementation of our insert function already does this implicitly
+because ties will be broken by the time of insertion
+*/
 
-Long random(Long a, Long b) {
-	return uniform_int_distribution<Long>(a , b)(rng);
+typedef long long Key;
+typedef int Prior;
+const Prior POOL = 1e9; //preferable in the order of n^2
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+Prior random(Prior a, Prior b) {
+	return uniform_int_distribution<Prior>(a, b)(rng);
 }
 
-struct Item{
-	Long key,prior;
-	Item *l, *r;
-	Item(){}
-	Item(Long key, Long prior) : key(key) , prior(prior), l (NULL), r(NULL){}
+struct Node {
+	Prior prior;
+	Key key;
+	Node* left;
+	Node* right;
+	Node(){}
+	Node(Key key, Prior prior): 
+		key(key), prior(prior), left(nullptr), right(nullptr) {}
 };
 
-typedef Item * pitem;
-
-void split(pitem t,  pitem &l, pitem &r, Long key){ //O(log N)
-	if(!t){
-		l = r = NULL;
-	}
-	else if(key < t->key){
-		split(t->l, l , t->l ,key), r = t;
-	}
-	else{
-		split(t->r, t->r , r ,key), l=t;
-	}
+void split(Node* t, Node* &l, Node* &r, Key key) { //O(log n)
+	//Splits tree `t` into two subtrees `l`, `r` such that 
+	//all keys of `l` are <= `key` and all keys of `r` are > `key`
+	if (!t) l = r = nullptr;
+	else if (t->key <= key) split(t->right, t->right, r, key), l = t;
+	else split(t->left, l, t->left, key), r = t;
 }
 
-void merge (pitem &t, pitem l , pitem r){ //O(log N)
-	if(!l || !r){
-		if(l) t=l;
-		else t = r;
-	}
-	else if(l -> prior > r->prior){
-		merge(l -> r, l -> r, r);
-		t = l;
-	}
-	else{
-		merge(r -> l, l , r->l);
-		t = r;
-	}
+void merge(Node* &t, Node* l, Node* r) { //O(log n)
+	//Merge trees `l` and `r` into tree `t`, assuming that
+	//all the keys on `l` are smaller than the keys on `r`
+	if (!l || !r) t = l ? l : r;
+	else if (l->prior > r->prior) merge(l->right, l->right, r), t = l;
+	else merge(r->left, l, r->left), t = r;
 }
 
-struct Treap{
-	pitem tree;
-
-	void insert(pitem &t, pitem it){ //O(log N) 
-		if(!t){
-			t = it;
-		}
-		else if(it->prior > t->prior){
-			split(t, it->l , it->r ,it->key);
-			t = it;
-		}	
-		else{
-			if(it->key < t->key){
-				insert( t->l , it);
-			}
-			else{
-				insert(t->r , it);
-			}
-		}
+struct Treap {
+	Node* tree;
+	Treap(): tree(nullptr){}
+	void insert(Node* &t, Node* node) { //O(log n) 
+		if (!t) t = node;
+		else if (node->prior > t->prior) {
+			split(t, node->left, node->right, node->key);
+			t = node;
+		} else if (node->key < t->key) insert(t->left, node);
+		else insert(t->right, node);
 	}
 	
-	void insert(Long X){ //O(log N) . !warning : complexity only works with different elements (it can be transform to a pair< value, frequency>)
-		Long r = random(0 , MAXINT);
-		pitem it = new Item(X , r);
-		insert(tree,it);
+	void insert(Key key) { //O(log n) 
+		insert(tree, new Node(key, random(0, POOL)));
+		//Another equivalent implementation with less code
+		/*Node *T1, *T2;
+		split(tree, T1, T2, X);
+		merge(T1, T1, new Node(X, random(0, POOL)));
+		merge(tree, T1, T2);*/
 	}
 	
-	void erase(pitem &t, Long key){ //O(log N)
-		if(!t){
-			return;
-		}
-		if(t-> key == key){
-			merge(t,t->l,t->r);
-		}
-		else{
-			if(key < t->key){
-				erase(t->l,key);
-			}
-			else{
-				erase(t->r,key);
-			}
-		}
+	void erase(Node* &t, Key key) { //O(log n)
+		if (!t) return;
+		if (t->key == key) merge(t, t->left, t->right);
+		else if (key < t->key) erase(t->left, key);
+		else erase(t->right, key);
 	}
 	
-	void erase(Long key){ //O(log N)
-		erase(tree,key);
+	void erase(Key key) { //O(log n)
+		//Erase only one element with given key
+		erase(tree, key);
+		//Erase all elements with given key
+		/*Node *T1, *T2, *T3;
+		split(tree, T1, T2, key - 1);
+		split(T2, T2, T3, key);
+		merge(tree, T1, T3);*/
 	}
 	
-	bool search(pitem t, Long key){ //O(log N)
-		if(!t){
-			return false;
-		}
-		
-		if(t->key == key){
-			return true;
-		}
-		else{
-			if(key < t->key){
-				return search(t->l,key);
-			}
-			else{
-				return search(t->r,key);
-			}
-		}
+	bool search(Node* t, Key key) { //O(log n)
+		if (!t) return false;
+		if (t->key == key) return true;
+		else if (key < t->key) return search(t->left, key);
+		else return search(t->right, key);
 	}
 	
-	bool search(Long key){ //O(log N)
-		return search(tree,key);
+	bool search(Key key) { //O(log n)
+		return search(tree, key);
 	}
 	
-	pitem unite( pitem l, pitem r) { //O( M log (N / M) )
-		if(!l || !r) {
-			return l ? l : r;
-		}
-		if(l -> prior < r -> prior) {
-			swap(l , r);
-		}
-		pitem lt,rt;
-		split(r , lt, rt , l -> key);
-		l -> l = unite(l -> l, lt);
-		l -> r = unite(l -> r, rt);
+	Node* unite(Node* l, Node* r) { //O(m log (n / m))
+		if (!l || !r) return l ? l : r;
+		if (l->prior < r->prior) swap(l , r);
+		Node* lt;
+		Node* rt;
+		split(r, lt, rt, l->key);
+		l->left = unite(l->left, lt);
+		l->right = unite(l->right, rt);
 		return l;
 	}
 
-	void unite(Treap t2) { //O( M log (N / M) )
-		tree = unite(tree , t2.tree);
+	void unite(Treap t2) { //O(m log (n / m))
+		tree = unite(tree, t2.tree);
 	}
-}tp;
-
-
-
-int main() {
-	return 0;
-}
+} treap;
--- DataStructure/Treap/treapSum.cpp
+++ DataStructure/Treap/treapSum.cpp
@@ -1,185 +1,96 @@
 #include <bits/stdc++.h>
-#define debug(x) cout << #x << " = " << x << endl
-#define REP(i,n) for(Long i = 0; i < (Long)n; i++)
-#define pb push_back
-
 using namespace std;
 
 typedef long long Long;
 
-const int MAXINT = 1e9;
+/*
+Operations:
+Insert(key, val): Insert a key with its value
+Erase(key): Erase all values with this key
+Query(l, r): Return the sum of values of all elements with key in [l, r]
 
-mt19937_64  rng(chrono::steady_clock::now().time_since_epoch().count());
+It's possible to replace "sum" with any associative operator
+*/
+typedef long long Key;
+typedef int Prior;
+const Prior POOL = 1e9; //preferable in the order of n^2
 
-Long random(Long a, Long b) {
-	return uniform_int_distribution<Long>(a , b)(rng);
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+Prior random(Prior a, Prior b) {
+	return uniform_int_distribution<Prior>(a, b)(rng);
 }
 
-struct Item{
-	Long key,prior,value,add;
-	Item *l, *r;
-	Item(){}
-	Item(Long key, Long prior, Long value) : key(key) , prior(prior), value(value), add(0),l (NULL), r(NULL){}
-	
-	void debugItem(string s){
-		cout << s << " = ( " << key  << " , " << value << " , "  << add << " ) " << endl;
-	}
+Long f(Long a, Long b) {
+	return a + b;
+}
+
+struct Node {
+	Prior prior;
+	Key key;
+	Long value, answer; 
+	//`sum` is the total sum of values in the subtree
+	Node* left;
+	Node* right;
+	Node(){}
+	Node(Key key, Prior prior, Long value): 
+		key(key), prior(prior), value(value), answer(0), left(nullptr), right(nullptr) {}
 };
 
-typedef Item * pitem;
-
-Long val(pitem it){
-	return it ? it->value + it->add : 0;
+Long get(Node* node) {
+	return node ? node->answer : 0;
 }
 
-void updateVal(pitem it){
-	if(it){
-		it->add =  val(it->l) + val(it->r);
+void recalc(Node* node) {
+	if (node) {
+		node->answer = f(get(node->left), node->value);
+		node->answer = f(node->answer, get(node->right));
 	}
 }
 
-void split(pitem t,  pitem &l, pitem &r, Long key){  //O(log N)
-	if(!t){
-		l = r = NULL;
-	}
-	else if(key <= t->key){
-		split(t->l, l , t->l ,key), r = t;
-	}
-	else{
-		split(t->r, t->r , r ,key), l=t;
-	}
-	updateVal(t);
+void split(Node* t, Node* &l, Node* &r, Key key) { //O(log n)
+	//Splits tree `t` into two subtrees `l`, `r` such that 
+	//all keys of `l` are <= `key` and all keys of `r` are > `key`
+	if (!t) l = r = nullptr;
+	else if (t->key <= key) split(t->right, t->right, r, key), l = t;
+	else split(t->left, l, t->left, key), r = t;
+	recalc(t);
 }
 
-void merge (pitem &t, pitem l , pitem r){  //O(log N)
-	if(!l || !r){
-		if(l) t=l;
-		else t = r;
-	}
-	else if(l -> prior > r->prior){
-		merge(l->r,l->r,r);
-		t = l;
-	}
-	else{
-		merge(r->l, l ,r->l);
-		t = r;
-	}
-	updateVal(t);
+void merge(Node* &t, Node* l, Node* r) { //O(log n)
+	//Merge trees `l` and `r` into tree `t`, assuming that
+	//all the keys on `l` are smaller than the keys on `r`
+	if (!l || !r) t = l ? l : r;
+	else if (l->prior > r->prior) merge(l->right, l->right, r), t = l;
+	else merge(r->left, l, r->left), t = r;
+	recalc(t);
 }
 
 struct Treap{
-	pitem tree;
+	Node* tree;
+	Treap(): tree(nullptr){}
 	
-	void insert(pitem &t, pitem it){ //O(log N) 
-		if(!t){
-			t = it;
-		}
-		else if(it->prior > t->prior){
-			split(t, it->l , it->r ,it->key);
-			t = it;
-		}	
-		else{
-			if(it->key < t->key){
-				insert( t->l , it);
-			}
-			else{
-				insert(t->r , it);
-			}
-		}
-		updateVal(t);
+	void insert(Key key, Long value) { //O(log n) 
+		Node *T1, *T2;
+		split(tree, T1, T2, key);
+		merge(T1, T1, new Node(key, random(0, POOL), value));
+		merge(tree, T1, T2);
 	}
 	
-	void insert(Long X, Long valor){ //O(log N) . !warning : complexity only works with different elements (it can be transform to a pair< value, frequency>)
-		Long r = random(0 , MAXINT);
-		pitem it = new Item(X, r, valor);
-		insert(tree,it);
+	void erase(Key key) { //O(log n)
+		Node *T1, *T2, *T3;
+		split(tree, T1, T2, key - 1);
+		split(T2, T2, T3, key);
+		merge(tree, T1, T3);
 	}
 	
-	void erase(pitem &t, Long key){  //O(log N)
-		if(!t){
-			return;
-		}
-		if(t-> key == key){
-			merge(t,t->l,t->r);
-		}
-		else{
-			if(key < t->key){
-				erase(t->l,key);
-			}
-			else{
-				erase(t->r,key);
-			}
-		}
-		updateVal(t);
+	Long query(Long l, Long r) {  //O(log n)
+		Node *T1, *T2, *T3;
+		split(tree, T1, T2, l - 1);
+		split(T2, T2, T3, r);
+		Long ans = get(T2);
+		merge(tree, T1, T2);
+		merge(tree, tree, T3);
+		return ans;
 	}
-	
-	void erase(Long key){  //O(log N)
-		erase(tree,key);
-	}
-	
-	Long query(Long A, Long B){  //O(log N)
-		pitem T1,T2,T3;
-		split(tree,T1,T2,A);
-		split(T2,T2,T3,B+1);
-		
-		Long resp = val(T2);
-	
-		merge(tree,T1,T2);
-		merge(tree,tree,T3);
-		
-		return resp;
-	}
-	
-	void debugTree(pitem t){
-		if(!t){
-			return;
-		}
-		(*t).debugItem("t");
-		if(t->l){
-			(*(t->l) ).debugItem("l");
-		}
-		if(t->r){
-			(*(t->r) ).debugItem("r");
-		}
-		cout << "**********************" << endl;
-		debugTree(t->l);
-		debugTree(t->r);
-		
-	}
-	
-	void debugTree(){
-		debugTree(tree);
-	}
-}tp;
-
-
-
-int main() {
-/*
-5
-2 5
-8 7
-1 -5
-6 15
-100 12
-*/
-	
-	Long n;
-	cin >> n;
-	REP(i,n){
-		Long x, valor;
-		cin >> x >> valor;
-		tp.insert(x,valor);
-	}
-	
-	while(true){
-		Long l,r;
-		cin >> l >> r;
-		debug(tp.query(l,r) );
-	}
-	
-	return 0;
-}
-
-
-
+} treap;
