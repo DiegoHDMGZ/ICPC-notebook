430406966169e1066ba9dce4d3132ad2b84182b8
Diego <dhurtadodemendoza@gmail.com>
2022-04-10 18:57:59 -0500

    fix style

--- Miscellaneous/clock.cpp
+++ Miscellaneous/clock.cpp
@@ -1,19 +1,8 @@
 #include <bits/stdc++.h>
-#define debug(x) cout << #x << " = " << x << endl
-#define REP(i , n) for(Long i = 0; i < (Long)n ; i++)
-#define pb push_back
-
 using namespace std;
 
 typedef long long Long;
 
 long double getTime(){
 	return chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now().time_since_epoch()).count();
-}
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	
-	return 0;
 }
